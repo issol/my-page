356844e375ae466ff019b2605ffbf14ed264bd39
seoulsaram <kasts91@naver.com>
2022-12-21 17:15:46 +0900

    add button variation on storybook

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,18 +1,35 @@
 import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
-import { Cta } from './Button'
+import { CtaButton } from './Button'
 
 export default {
   title: 'Components/Button',
-  component: Cta
+  component: CtaButton
 
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-} as ComponentMeta<typeof Cta>
-const Template: ComponentStory<typeof Cta> = args => <Cta {...args} />
+} as ComponentMeta<typeof CtaButton>
+const Template: ComponentStory<typeof CtaButton> = args => <CtaButton {...args} />
 
-export const Outlined = Template.bind({})
-Outlined.args = {
+export const Primary = Template.bind({})
+Primary.args = {
   children: 'Button',
   color: 'primary'
+}
+export const Secondary = Template.bind({})
+Secondary.args = {
+  children: 'Button',
+  color: 'secondary'
+}
+export const Round = Template.bind({})
+Round.args = {
+  children: 'Button',
+  color: 'primary',
+  shape: 'round'
+}
+export const Disabled = Template.bind({})
+Disabled.args = {
+  children: 'Button',
+  color: 'primary',
+  disabled: true
 }
--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -6,7 +6,7 @@ import pal from '../@core/theme/palette'
 import { PaletteType } from '../@core/theme/palette/type'
 
 interface CtaButtonProps {
-  shape?: 'default' | 'square'
+  shape?: 'default' | 'round'
   color?: 'primary' | 'secondary' | 'primary-border' | 'secondary-border' | 'error'
   width?: string
   fontSize?: string
@@ -19,14 +19,14 @@ interface ButtonProps {
   palette: PaletteType
 }
 
-const CtaButton = styled.button<CtaButtonProps & ButtonProps>`
+const CtaButtonStyle = styled.button<CtaButtonProps & ButtonProps>`
   cursor: pointer;
   width: ${({ width }) => width ?? 'unset'};
   display: flex;
   gap: 6px;
   align-items: center;
   justify-content: center;
-  border-radius: ${({ shape }) => (shape === 'square' ? '4px' : '30px')};
+  border-radius: ${({ shape }) => (shape === 'round' ? '30px' : '4px')};
   color: #fff;
   border: none;
   ${({ color, palette }) =>
@@ -45,9 +45,8 @@ const CtaButton = styled.button<CtaButtonProps & ButtonProps>`
   ${({ color, palette }) =>
     color === 'secondary' &&
     `
-      background: transparent;
-      border: 1px solid ${palette.secondary.main};
-      color: ${palette.secondary.main};
+      background: ${palette.secondary.main};
+      color: #ffffff;
     `}
   ${({ color, palette }) =>
     color === 'secondary-border' &&
@@ -63,7 +62,7 @@ const CtaButton = styled.button<CtaButtonProps & ButtonProps>`
       border: 1px solid ${palette.error.main};
       color: ${palette.error.main};
     `}
-  padding: 12px 24px;
+  padding: 8px 12px;
   font-size: ${({ fontSize }) => (fontSize ? fontSize : '1rem')};
   font-weight: 700;
   :hover {
@@ -111,16 +110,15 @@ const CtaButton = styled.button<CtaButtonProps & ButtonProps>`
     }
   }
   @media ${device.mobile} {
-    padding: 13px 26px;
     width: ${({ mobileWidth, width }) => (mobileWidth ? mobileWidth : width ? width : 'unset')};
   }
 `
-export const Cta = ({ children, ...rest }: CtaButtonProps) => {
+export const CtaButton = ({ children, ...rest }: CtaButtonProps) => {
   const palette = pal('light', 'default')
 
   return (
-    <CtaButton {...rest} palette={palette}>
+    <CtaButtonStyle {...rest} palette={palette}>
       {children}
-    </CtaButton>
+    </CtaButtonStyle>
   )
 }
