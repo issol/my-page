c0789bd6c8cefbd1b6392aed13eca104ca796f12
seoulsaram <kasts91@naver.com>
2023-01-02 14:50:27 +0900

    add typo story

---
+++ src/stories/Typography.stories.tsx
@@ -0,0 +1,68 @@
+import React, { ReactNode } from 'react'
+import { ComponentMeta } from '@storybook/react'
+import { Link, Typography } from '@mui/material'
+
+export default {
+  title: 'Typography',
+  component: Typography,
+  argTypes: {
+    align: {
+      control: { type: 'select' },
+      defaultValue: 'h1',
+      options: ['inherit', 'left', 'center', 'right', 'justify'],
+    },
+    children: {
+      control: { type: 'text' },
+      defaultValue: 'Typography',
+    },
+    noWrap: {
+      description:
+        'true이면 텍스트가 wrap되지 않고 텍스트 오버플로 줄임표로 잘립니다.',
+      defaultValue: true,
+      control: { type: 'boolean' },
+    },
+    paragraph: {
+      description: 'true이면 이 요소는 paragraph 엘리먼트가 됩니다.',
+      defaultValue: false,
+      control: { type: 'boolean' },
+    },
+    variant: {
+      description: 'true이면 이 요소는 paragraph 엘리먼트가 됩니다.',
+      defaultValue: 'h1',
+      control: { type: 'select' },
+      options: [
+        'h1',
+        'h2',
+        'h3',
+        'h4',
+        'h5',
+        'h6',
+        'subtitle1',
+        'subtitle2',
+        'body1',
+        'body2',
+        'inherit',
+      ],
+    },
+    sx: {
+      description: `시스템 오버라이드와 추가 css 스타일 정의 할수 있음. ex: sx={{ display: 'flex', alignItems: 'center' }}`,
+      defaultValue: { mb: 2 },
+      control: { type: 'object' },
+    },
+  },
+} as ComponentMeta<typeof Typography>
+
+export const Default = (args: typeof Typography) => {
+  /* @ts-ignore */
+  return <Typography {...args}>{args?.children}</Typography>
+}
+
+export const LinkTypo = () => {
+  return (
+    <Link href='/'>
+      import Link component from{' '}
+      <code>{`import { Link } from '@mui/material`}</code>
+      mui의 Link 컴포넌트를 사용하면 링크의 컬러로 theme이 사용됨
+    </Link>
+  )
+}
