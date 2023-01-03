07b1a2c8e6adcc47f4dd53bba4f5a9f1d48f9d02
seoulsaram <kasts91@naver.com>
2023-01-03 11:03:12 +0900

    fix modal story

--- src/stories/dialog/Modal.stories.tsx
+++ src/stories/dialog/Modal.stories.tsx
@@ -1,8 +1,11 @@
-import React, { ReactNode, useContext } from 'react'
 import { ComponentMeta } from '@storybook/react'
+
+import React, { ReactNode, useContext } from 'react'
 import { ModalContext } from 'src/context/ModalContext'
 import SimpleModal from 'src/@core/components/modal'
-import { Alert, Button, Divider } from '@mui/material'
+
+import { Alert, Grid, Button } from '@mui/material'
+import CardSnippet from 'src/@core/components/card-snippet'
 
 export default {
   title: 'Dialog/Modal',
@@ -34,34 +37,80 @@ type Props = {
 }
 export const Default = (args: Props) => {
   const { setModal } = useContext(ModalContext)
+
   function handleOnClick() {
     setModal(<SimpleModal {...args} msg={args.msg} />)
   }
   return (
     <div>
-      <Alert severity='info'>
-        Modal은 <code>ModalContext</code>의 <code>setModal</code> 메소드를 통해
-        간단하게 모달을 열 수 있습니다.
-        <br />
-        모달을 미리 페이지에 심어두고 open값을 조작할 필요 없이, <br />
-        모달이 필요한 시점에 <code>setModal</code>함수에 <code>ReactNode</code>
-        타입의 엘리먼트를 전달해주면 됩니다.
-        <br />
-        <strong>
-          <code>setModal</code>의 단점은, 부모 컴포넌트로부터 물려받은 state의
-          값이 변할 때마다 동기적으로 그 값을 보여줄 수 없다는 것입니다.
-          <br />
-          왜냐하면 부모로부터 물려받은 state의 값이
-          <code>setModal</code>을 할 시점의 state로 고정되기 때문입니다.
-          <br />
-          이것을 방지하려면 Modal컴포넌트 내부에 state를 심는 방법을 사용해야
-          합니다.
-        </strong>
-      </Alert>
-      <Divider />
-      <Button variant='contained' onClick={handleOnClick}>
-        Show Modal
-      </Button>
+      <Grid item xs={12}>
+        <CardSnippet
+          title='Modal'
+          code={{
+            tsx: source,
+            jsx: source,
+          }}
+        >
+          <Alert severity='info'>
+            Modal은 <code>ModalContext</code>의 <code>setModal</code> 메소드를
+            통해 간단하게 모달을 열 수 있습니다.
+            <br />
+            모달을 미리 페이지에 심어두고 open값을 조작할 필요 없이, <br />
+            모달이 필요한 시점에 <code>setModal</code>함수에{' '}
+            <code>ReactNode</code>
+            타입의 엘리먼트를 전달해주면 됩니다.
+            <br />
+            <strong>
+              <code>setModal</code>의 단점은, 부모 컴포넌트로부터 물려받은
+              state의 값이 변할 때마다 동기적으로 그 값을 보여줄 수 없다는
+              것입니다.
+              <br />
+              왜냐하면 부모로부터 물려받은 state의 값이
+              <code>setModal</code>을 할 시점의 state로 고정되기 때문입니다.
+              <br />
+              이것을 방지하려면 Modal컴포넌트 내부에 state를 심는 방법을
+              사용해야 합니다.
+            </strong>
+          </Alert>
+          <Button
+            variant='contained'
+            onClick={handleOnClick}
+            style={{ marginTop: '30px' }}
+          >
+            Show Modal
+          </Button>
+        </CardSnippet>
+      </Grid>
     </div>
   )
 }
+
+const source = (
+  <pre className='language-jsx'>
+    <code className='language-jsx'>{`
+import React, { ReactNode, useContext } from 'react'
+import { ModalContext } from 'src/context/ModalContext'
+import SimpleModal from 'src/@core/components/modal'
+import { Button } from '@mui/material'
+
+export const Default = (args: Props) => {
+  const { setModal } = useContext(ModalContext)
+
+  function handleOnClick() {
+    setModal(<SimpleModal {...args} msg={args.msg} />)
+  }
+  return (
+    <Button
+      variant='contained'
+      onClick={handleOnClick}
+      style={{ marginTop: '30px' }}
+    >
+      Show Modal
+    </Button>
+  )
+}
+
+export default BarExample;
+`}</code>
+  </pre>
+)
