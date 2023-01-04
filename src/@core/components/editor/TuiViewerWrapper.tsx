b2ae028cdd208c4b7490d642eecb629dede3afca
seoulsaram <kasts91@naver.com>
2023-01-04 09:25:51 +0900

    delete custom editor components & editor pages

--- src/@core/components/editor/TuiViewerWrapper.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import { Viewer, ViewerProps } from '@toast-ui/react-editor'
-
-export default (props: ViewerProps) => <Viewer {...props} initialValue={props.initialValue} />
