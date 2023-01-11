57d34bacd0face122103cb8d803019f7b893c310
seoulsaram <kasts91@naver.com>
2023-01-11 21:04:20 +0900

    add personalInfo const

--- src/shared/const/personalInfo.ts
+++ src/shared/const/personalInfo.ts
@@ -4,3 +4,53 @@ export const Pronunciation = [
   { value: 'THEY', label: 'they/them/their' },
   { value: 'NONE', label: 'Prefer not to answer' },
 ] as const
+
+export const JobList = [
+  { label: 'Documents/Text', value: 'document_text' },
+  { label: 'DTP', value: 'dtp' },
+  { label: 'Dubbing', value: 'dubbing' },
+  { label: 'OTT/Subtitle', value: 'ott_subtitle' },
+  { label: 'Webcomics', value: 'webcomics' },
+  { label: 'Webnovel', value: 'webnovel' },
+  { label: 'YouTube', value: 'youtube' },
+]
+
+export const RolePair = {
+  document_text: [
+    { label: 'QCer', value: 'QCer' },
+    { label: 'Translator', value: 'Translator' },
+  ],
+  dtp: [
+    { label: 'DTPer', value: 'DTPer' },
+    { label: 'DTP QCer', value: 'DTP QCer' },
+  ],
+  dubbing: [
+    { label: 'Audio description QCer', value: 'Audio description QCer' },
+    { label: 'Dubbing QCer', value: 'Dubbing QCer' },
+    { label: 'Dubbing script translator', value: 'Dubbing script translator' },
+  ],
+  ott_subtitle: [
+    { label: 'SDH author', value: 'SDH author' },
+    { label: 'SDH QCer', value: 'SDH QCer' },
+    { label: 'Subtitle author', value: 'Subtitle author' },
+    { label: 'Subtitle QCer', value: 'Subtitle QCer' },
+    { label: 'Template author', value: 'Template author' },
+    { label: 'Template QCer', value: 'Template QCer' },
+    { label: 'Supp author', value: 'Supp author' },
+    { label: 'Supp QCer', value: 'Supp QCer' },
+  ],
+  webcomics: [
+    { label: 'Proofreader', value: 'Proofreader' },
+    { label: 'Webcomics QCer', value: 'Webcomics QCer' },
+    { label: 'Webcomic translator', value: 'Webcomic translator' },
+  ],
+  webnovel: [
+    { label: 'Wenovel QCer', value: 'Wenovel QCer' },
+    { label: 'Webnovel translator', value: 'Webnovel translator' },
+  ],
+  youtube: [
+    { label: 'Subtitle QCer', value: 'Subtitle QCer' },
+    { label: 'Subtitle author', value: 'Subtitle author' },
+    { label: 'Translator', value: 'Translator' },
+  ],
+}
