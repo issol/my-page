4df35e9b3420b2d332be83152046c809f7d1e970
seoulsaram <kasts91@naver.com>
2023-01-27 14:51:15 +0900

    update README

--- README.md
+++ README.md
@@ -33,6 +33,13 @@
 - `src/shared/auth/storage.ts`
   - 브라우저 storage를 crud하는 함수들을 모아둔 곳. (ex: userData, accessToken..)
 
+**접근제어 관리**
+
+- `src/configs/acl.ts`
+  - 유저의 ability 인스턴스를 함수가 위치 (유저의 can, cannot 정의)
+- `src/@core/components/auth/AclGuard.tsx`
+  - `acl.ts`의 ability를 정의하는 함수를 사용하고, 정의된 ability 객체를 context로 제공하고 있는 부분.
+
 ---
 
 ## docs
--- src/configs/acl.ts
+++ src/configs/acl.ts
@@ -1,30 +1,6 @@
 import { AbilityBuilder, Ability } from '@casl/ability'
 import { PermissionObjectType } from 'src/context/types'
 
-/* TODO :
-서버에서 받는 값
-  - user permission에 대한 값
-  - permission에 대한 정보 object (Array<{name:"RE008", description:"Refresh 엑세스 토큰", accessPoint:'/api/..', method:'GET'}>)
-
-
-1. defineRulesFor
-  - user permission을 받아 조건문으로 각 permission 당 어떤 것을 허용해줄 지 정의해야 함
-  - ex: RE008은 user READ, UPDATE가 가능
-    if(permission.includes(RE008)) {
-      can(['read','update'], user)
-    }
-  - 만약 서버에서 permission 오브젝트를 아래와 같이 준다면
-    {
-      RE008: {description :"User", can:["read","update"]}
-    }
-
-    permission.forEach((per)=> {
-      can(perObj[per].can, perObj[per].description)
-    })
-    으로 가능.
-  
-*/
-
 export type Action = 'all' | 'create' | 'read' | 'update' | 'delete'
 export type Subjects = string
 
@@ -46,7 +22,6 @@ export type PolicyType = {
 }
 
 const defineRulesFor = (userPermission: PermissionObjectType) => {
-  console.log(userPermission)
   const { can, rules } = new AbilityBuilder(AppAbility)
   if (userPermission.length) {
     userPermission.forEach(permission => {
