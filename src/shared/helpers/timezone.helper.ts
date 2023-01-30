57f93eae4d553bf03219ef225f08350bc8319520
seoulsaram <kasts91@naver.com>
2023-01-31 02:01:24 +0900

    add date helpers

--- src/shared/helpers/timezone.helper.ts
+++ src/shared/helpers/timezone.helper.ts
@@ -11,3 +11,19 @@ export const getGmtTime = (code: string) => {
   const timezoneName = timezones.countries[code].zones[0]
   return `(GMT${moment.tz(timezoneName).format('Z')}) ${timezoneName}`
 }
+
+export function getTimezone(value: any) {
+  if (!value) return ''
+  const timeZone = moment.tz.guess()
+  let date
+  //this is for safari response
+  if (isNaN(new Date(value).valueOf())) {
+    date = new Date(value.replace(/-/g, '/'))
+  } else {
+    date = new Date(value)
+  }
+
+  const timeZoneOffset = date.getTimezoneOffset()
+  const timeZoneAbbr = moment.tz.zone(timeZone)!.abbr(timeZoneOffset)
+  return timeZoneAbbr
+}
