6dba036e1e3b66be3802364d1205ddd7ef09d923
seoulsaram <kasts91@naver.com>
2023-01-30 17:48:50 +0900

    add information modal

--- src/pages/onboarding/contract-forms/index.tsx
+++ src/pages/onboarding/contract-forms/index.tsx
@@ -2,87 +2,143 @@ import { Button, Card, Grid, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 
 import styled from 'styled-components'
+import Icon from 'src/@core/components/icon'
+import { useContext } from 'react'
+import { ModalContext } from 'src/context/ModalContext'
 export default function ContractForm() {
+  const { setModal } = useContext(ModalContext)
+
+  function onInfoClick() {
+    setModal(
+      <ModalContainer>
+        <ModalBody>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <img
+              src='/images/icons/project-icons/status-alert-info.png'
+              width={60}
+              height={60}
+              alt='role select error'
+            />
+            <Icon
+              icon='mdi:close'
+              style={{
+                opacity: '0.7',
+                position: 'absolute',
+                top: '24px',
+                right: '20px',
+              }}
+              cursor='pointer'
+              onClick={() => setModal(null)}
+            />
+          </Box>
+
+          <Typography variant='h6'>Contracts information</Typography>
+          <Typography variant='body2'>
+            <Ul>
+              <li>
+                Pros sign the NDA only once before taking their first
+                Certification test.
+              </li>
+              <li>
+                Pros sign the Privacy contract and Freelancer contract after the
+                onboarding.
+              </li>
+            </Ul>
+          </Typography>
+        </ModalBody>
+      </ModalContainer>,
+    )
+  }
   return (
     <Grid xs={12} container>
-      <Card sx={{ width: '100%', margin: '0 96px', padding: '80px' }}>
-        <Typography variant='h5' sx={{ textAlign: 'center' }}>
-          Contract forms
-        </Typography>
-        <Box
-          sx={{
-            display: 'grid',
-            gridTemplateColumns: '1fr 1fr 1fr',
-            gap: '24px',
-          }}
-          mt='24px'
-        >
-          {/* NDA */}
-          <StyledBox>
-            <img
-              width={58}
-              height={58}
-              src='/images/icons/etc/icon-keyboad.png'
-              alt='NDA'
-            />
-            <Typography
-              variant='h5'
-              sx={{ textAlign: 'center', margin: '12px 0' }}
-            >
-              NDA
-            </Typography>
-            <Typography variant='body2' sx={{ textAlign: 'center' }}>
-              Pros will sign the NDA before taking the certification test.
-            </Typography>
-            <Box display='flex' gap='8px' mt='12px'>
-              <Button variant='outlined'>KOR</Button>
-              <Button variant='outlined'>ENG</Button>
-            </Box>
-          </StyledBox>
-          {/* Privacy */}
-          <StyledBox>
-            <img
-              width={58}
-              height={58}
-              src='/images/icons/etc/icon-suitcase.png'
-              alt='NDA'
-            />
-            <Typography
-              variant='h5'
-              sx={{ textAlign: 'center', margin: '12px 0' }}
-            >
-              Privacy contract
-            </Typography>
-            <Typography variant='body2' sx={{ textAlign: 'center' }}>
-              Pros will sign the Privacy contract after being onboarded.
-            </Typography>
-            <Box display='flex' gap='8px' mt='12px'>
-              <Button variant='outlined'>KOR</Button>
-              <Button variant='outlined'>ENG</Button>
-            </Box>
-          </StyledBox>
-          {/* Freelancer */}
-          <StyledBox>
-            <img
-              width={58}
-              height={58}
-              src='/images/icons/etc/icon-user.png'
-              alt='NDA'
-            />
-            <Typography
-              variant='h5'
-              sx={{ textAlign: 'center', margin: '12px 0' }}
-            >
-              Freelancer contract
-            </Typography>
-            <Typography variant='body2' sx={{ textAlign: 'center' }}>
-              Pros will sign the Freelancer contract after being onboarded.
-            </Typography>
-            <Box display='flex' gap='8px' mt='12px'>
-              <Button variant='outlined'>KOR</Button>
-              <Button variant='outlined'>ENG</Button>
-            </Box>
-          </StyledBox>
+      <Card sx={{ width: '100%', margin: '0 96px', padding: '22px' }}>
+        <Box sx={{ textAlign: 'right' }}>
+          <Icon
+            icon='mdi:information-outline'
+            style={{ opacity: '0.7' }}
+            cursor='pointer'
+            onClick={onInfoClick}
+          />
+        </Box>
+        <Box sx={{ padding: '58px' }}>
+          <Typography variant='h5' sx={{ textAlign: 'center' }}>
+            Contract forms
+          </Typography>
+          <Box
+            sx={{
+              display: 'grid',
+              gridTemplateColumns: '1fr 1fr 1fr',
+              gap: '24px',
+            }}
+            mt='24px'
+          >
+            {/* NDA */}
+            <StyledBox>
+              <img
+                width={58}
+                height={58}
+                src='/images/icons/etc/icon-keyboad.png'
+                alt='NDA'
+              />
+              <Typography
+                variant='h5'
+                sx={{ textAlign: 'center', margin: '12px 0' }}
+              >
+                NDA
+              </Typography>
+              <Typography variant='body2' sx={{ textAlign: 'center' }}>
+                Pros will sign the NDA before taking the certification test.
+              </Typography>
+              <Box display='flex' gap='8px' mt='12px'>
+                <Button variant='outlined'>KOR</Button>
+                <Button variant='outlined'>ENG</Button>
+              </Box>
+            </StyledBox>
+            {/* Privacy */}
+            <StyledBox>
+              <img
+                width={58}
+                height={58}
+                src='/images/icons/etc/icon-suitcase.png'
+                alt='NDA'
+              />
+              <Typography
+                variant='h5'
+                sx={{ textAlign: 'center', margin: '12px 0' }}
+              >
+                Privacy contract
+              </Typography>
+              <Typography variant='body2' sx={{ textAlign: 'center' }}>
+                Pros will sign the Privacy contract after being onboarded.
+              </Typography>
+              <Box display='flex' gap='8px' mt='12px'>
+                <Button variant='outlined'>KOR</Button>
+                <Button variant='outlined'>ENG</Button>
+              </Box>
+            </StyledBox>
+            {/* Freelancer */}
+            <StyledBox>
+              <img
+                width={58}
+                height={58}
+                src='/images/icons/etc/icon-user.png'
+                alt='NDA'
+              />
+              <Typography
+                variant='h5'
+                sx={{ textAlign: 'center', margin: '12px 0' }}
+              >
+                Freelancer contract
+              </Typography>
+              <Typography variant='body2' sx={{ textAlign: 'center' }}>
+                Pros will sign the Freelancer contract after being onboarded.
+              </Typography>
+              <Box display='flex' gap='8px' mt='12px'>
+                <Button variant='outlined'>KOR</Button>
+                <Button variant='outlined'>ENG</Button>
+              </Box>
+            </StyledBox>
+          </Box>
         </Box>
       </Card>
     </Grid>
@@ -102,4 +158,24 @@ const StyledBox = styled(Box)`
   align-items: center;
   border: 1px solid rgba(76, 78, 100, 0.12);
   border-radius: 10px;
+`
+
+const Ul = styled.ul`
+  padding-left: 24px;
+  text-align: left;
+`
+
+const ModalContainer = styled(Box)`
+  max-width: 350px;
+  position: relative;
+  padding: 24px;
+  text-align: center;
+  background: #ffffff;
+  border-radius: 14px;
+`
+const ModalBody = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
 `
