8ccd92572b224951082280c0a649f9175f8f89b0
seoulsaram <kasts91@naver.com>
2023-02-10 15:32:55 +0900

    test용 permission 주석처리

--- src/store/permission/index.ts
+++ src/store/permission/index.ts
@@ -18,37 +18,37 @@ export const getPermission = createAsyncThunk(
     try {
       const { data } = await axios.get(`/api/enough/a/role/map`)
       // console.log('permission : ', data)
-      return [
-        {
-          subject: 'members',
-          can: ['read', 'create', 'update', 'delete'],
-        },
-        {
-          subject: 'permission_request',
-          can: ['read', 'create', 'update', 'delete'],
-        },
-        {
-          subject: 'personalInfo_pro',
-          can: ['read', 'create', 'update', 'delete'],
-        },
-        {
-          subject: 'personalInfo_manager',
-          can: ['read', 'create', 'update', 'delete'],
-        },
-        /* TODO: 임시 permission */
-        {
-          subject: 'onboarding',
-          can: ['read', 'create', 'update', 'delete'],
-        },
-        {
-          subject: 'client_guideline',
-          can: ['read', 'create', 'update', 'delete'],
-        },
-        {
-          subject: 'contract',
-          can: ['read', 'create', 'update', 'delete'],
-        },
-      ]
+      // return [
+      //   {
+      //     subject: 'members',
+      //     can: ['read', 'create', 'update', 'delete'],
+      //   },
+      //   {
+      //     subject: 'permission_request',
+      //     can: ['read', 'create', 'update', 'delete'],
+      //   },
+      //   {
+      //     subject: 'personalInfo_pro',
+      //     can: ['read', 'create', 'update', 'delete'],
+      //   },
+      //   {
+      //     subject: 'personalInfo_manager',
+      //     can: ['read', 'create', 'update', 'delete'],
+      //   },
+      //   /* TODO: 임시 permission */
+      //   {
+      //     subject: 'onboarding',
+      //     can: ['read', 'create', 'update', 'delete'],
+      //   },
+      //   {
+      //     subject: 'client_guideline',
+      //     can: ['read', 'create', 'update', 'delete'],
+      //   },
+      //   {
+      //     subject: 'contract',
+      //     can: ['read', 'create', 'update', 'delete'],
+      //   },
+      // ]
       return data
     } catch (e: any) {
       throw new Error('getPermission error : ', e)
