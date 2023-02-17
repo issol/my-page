bfe15138d0dc8ba06a8eeff2584015c62e811a2c
LerielKim <85856680+LerielKim@users.noreply.github.com>
2023-02-17 16:51:14 +0900

    [Fix] experience of years value

--- src/shared/const/personalInfo.ts
+++ src/shared/const/personalInfo.ts
@@ -64,7 +64,7 @@ export const RolePair = {
 
 export const ExperiencedYears = [
   { value: 'No experience', label: 'No experience' },
-  { value: '1-2 years', label: '1-2 years' },
+  { value: '1-2 year(s)', label: '1-2 year(s)' },
   { value: '3-5 years', label: '3-5 years' },
   { value: '6-9 years', label: '6-9 years' },
   { value: '10+ years', label: '10+ years' },
