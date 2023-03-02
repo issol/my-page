ea0c8a2d63b6bb365cbefbdab7ec97aca7d746e7
seoulsaram <kasts91@naver.com>
2023-03-02 11:40:09 +0900

    add jobPosting filter

---
+++ src/pages/jobPosting/components/filter.tsx
@@ -0,0 +1,300 @@
+// ** React imports
+import { useEffect, useState } from 'react'
+
+// ** MUI Imports
+import FormControl from '@mui/material/FormControl'
+import TextField from '@mui/material/TextField'
+import Autocomplete from '@mui/material/Autocomplete'
+import { Box } from '@mui/system'
+import Collapse from '@mui/material/Collapse'
+import CardContent from '@mui/material/CardContent'
+import {
+  Button,
+  Card,
+  CardHeader,
+  Checkbox,
+  Grid,
+  IconButton,
+  InputLabel,
+  OutlinedInput,
+} from '@mui/material'
+import { useTheme } from '@mui/material/styles'
+
+// ** Styled Component Import
+import DatePickerWrapper from 'src/@core/styles/libs/react-datepicker'
+import DatePicker, { ReactDatePickerProps } from 'react-datepicker'
+import CustomInput from 'src/views/forms/form-elements/pickers/PickersCustomInput'
+
+import styled from 'styled-components'
+
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+
+// **values
+import { JobList, RoleList } from 'src/shared/const/common'
+import { ExperiencedYears } from 'src/shared/const/personalInfo'
+
+import { getGloLanguage } from 'src/shared/transformer/language.transformer'
+
+// ** types
+import { FilterOmitType } from '..'
+
+type Filter = { value: string; label: string }
+type Props = {
+  filter: FilterOmitType
+  setFilter: <T extends FilterOmitType>(v: T) => void
+  search: () => void
+  onReset: () => void
+  jobTypeOption: Array<Filter>
+  roleOption: Array<Filter>
+}
+
+export default function Filters({
+  filter,
+  setFilter,
+  search,
+  onReset,
+  jobTypeOption,
+  roleOption,
+}: Props) {
+  const languageList = getGloLanguage()
+  const [collapsed, setCollapsed] = useState<boolean>(true)
+
+  const theme = useTheme()
+  const { direction } = theme
+  const popperPlacement: ReactDatePickerProps['popperPlacement'] =
+    direction === 'ltr' ? 'bottom-start' : 'bottom-end'
+
+  function filterValue(option: any, keyName: keyof FilterOmitType) {
+    return !filter[keyName]
+      ? { value: '', label: '' }
+      : option.filter(
+          (item: { value: string; label: string }) =>
+            filter[keyName] === item.value,
+        )[0]
+  }
+
+  return (
+    <Grid item xs={12}>
+      <Card>
+        <CardHeader
+          title='Search Filters'
+          action={
+            <IconButton
+              size='small'
+              aria-label='collapse'
+              sx={{ color: 'text.secondary' }}
+              onClick={() => setCollapsed(!collapsed)}
+            >
+              <Icon
+                fontSize={20}
+                icon={!collapsed ? 'mdi:chevron-down' : 'mdi:chevron-up'}
+              />
+            </IconButton>
+          }
+        />
+        <Collapse in={collapsed}>
+          <CardContent>
+            <Grid container spacing={6} rowSpacing={4}>
+              <Grid item xs={12} sm={6} md={4}>
+                <FormControl fullWidth>
+                  <FormControl fullWidth>
+                    <Autocomplete
+                      autoHighlight
+                      fullWidth
+                      options={!jobTypeOption.length ? JobList : jobTypeOption}
+                      value={filterValue(JobList, 'jobType')}
+                      onChange={(e, v) =>
+                        setFilter({
+                          ...filter,
+                          jobType: v?.value ?? '',
+                        })
+                      }
+                      filterSelectedOptions
+                      id='jobType'
+                      getOptionLabel={option => option.label}
+                      renderInput={params => (
+                        <TextField
+                          {...params}
+                          label='Job type'
+                          placeholder='Job type'
+                        />
+                      )}
+                      renderOption={(props, option, { selected }) => (
+                        <li {...props}>
+                          <Checkbox checked={selected} sx={{ mr: 2 }} />
+                          {option.label}
+                        </li>
+                      )}
+                    />
+                  </FormControl>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <FormControl fullWidth>
+                  <Autocomplete
+                    autoHighlight
+                    fullWidth
+                    options={!roleOption.length ? RoleList : roleOption}
+                    value={filterValue(RoleList, 'role')}
+                    onChange={(e, v) =>
+                      setFilter({
+                        ...filter,
+                        role: v?.value ?? '',
+                      })
+                    }
+                    filterSelectedOptions
+                    id='role'
+                    getOptionLabel={option => option.label}
+                    renderInput={params => (
+                      <TextField {...params} label='Role' placeholder='Role' />
+                    )}
+                    renderOption={(props, option, { selected }) => (
+                      <li {...props}>
+                        <Checkbox checked={selected} sx={{ mr: 2 }} />
+                        {option.label}
+                      </li>
+                    )}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <FormControl fullWidth>
+                  <Autocomplete
+                    autoHighlight
+                    fullWidth
+                    options={ExperiencedYears}
+                    value={filterValue(ExperiencedYears, 'yearsOfExperience')}
+                    onChange={(e, v) =>
+                      setFilter({
+                        ...filter,
+                        yearsOfExperience: v?.value ?? '',
+                      })
+                    }
+                    filterSelectedOptions
+                    id='yearsOfExperience'
+                    getOptionLabel={option => option.label}
+                    renderInput={params => (
+                      <TextField
+                        {...params}
+                        label='Years of experience'
+                        placeholder='Years of experience'
+                      />
+                    )}
+                    renderOption={(props, option, { selected }) => (
+                      <li {...props}>
+                        <Checkbox checked={selected} sx={{ mr: 2 }} />
+                        {option.label}
+                      </li>
+                    )}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={4} md={4}>
+                <FormControl fullWidth>
+                  <Autocomplete
+                    autoHighlight
+                    fullWidth
+                    options={languageList}
+                    value={filterValue(languageList, 'source')}
+                    onChange={(e, v) => {
+                      setFilter({
+                        ...filter,
+                        source: v?.value ?? '',
+                      })
+                    }}
+                    filterSelectedOptions
+                    id='source'
+                    getOptionLabel={option => option.label}
+                    renderInput={params => (
+                      <TextField
+                        {...params}
+                        label='Source'
+                        placeholder='Source'
+                      />
+                    )}
+                    renderOption={(props, option, { selected }) => (
+                      <li {...props} key={option.value}>
+                        <Checkbox checked={selected} sx={{ mr: 2 }} />
+                        {option.label}
+                      </li>
+                    )}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={4} md={4}>
+                <FormControl fullWidth>
+                  <Autocomplete
+                    autoHighlight
+                    fullWidth
+                    options={languageList}
+                    value={filterValue(languageList, 'target')}
+                    onChange={(e, v) =>
+                      setFilter({
+                        ...filter,
+                        target: v?.value ?? '',
+                      })
+                    }
+                    filterSelectedOptions
+                    id='target'
+                    getOptionLabel={option => option.label}
+                    renderInput={params => (
+                      <TextField
+                        {...params}
+                        label='Target'
+                        placeholder='Target'
+                      />
+                    )}
+                    renderOption={(props, option, { selected }) => (
+                      <li {...props} key={option.value}>
+                        <Checkbox checked={selected} sx={{ mr: 2 }} />
+                        {option.label}
+                      </li>
+                    )}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={4} md={4}>
+                <FormControl fullWidth>
+                  <DatePickerWrapper>
+                    <CustomDatePicker
+                      selected={
+                        filter.dueDate ? new Date(filter.dueDate) : null
+                      }
+                      id='dueDate'
+                      popperPlacement={popperPlacement}
+                      onChange={date =>
+                        setFilter({ ...filter, dueDate: date?.toString() })
+                      }
+                      placeholderText='Due date'
+                      customInput={<CustomInput icon='calendar' />}
+                    />
+                  </DatePickerWrapper>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <Box display='flex' justifyContent='flex-end' gap='15px'>
+                  <Button
+                    variant='outlined'
+                    size='medium'
+                    color='secondary'
+                    onClick={onReset}
+                  >
+                    Reset
+                  </Button>
+                  <Button variant='contained' size='medium' onClick={search}>
+                    Search
+                  </Button>
+                </Box>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Collapse>
+      </Card>
+    </Grid>
+  )
+}
+
+const CustomDatePicker = styled(DatePicker)`
+  z-index: 100000;
+`
--- src/pages/jobPosting/index.tsx
+++ src/pages/jobPosting/index.tsx
@@ -1,5 +1,111 @@
+import { useEffect, useState } from 'react'
+// ** mui imports
+import { Grid, Typography } from '@mui/material'
+import PageHeader from 'src/@core/components/page-header'
+
+// ** third parties
+import isEqual from 'lodash/isEqual'
+
+// ** values
+import { ProJobPair, ProRolePair } from 'src/shared/const/common'
+import Filters from './components/filter'
+
+export type FilterType = {
+  jobType: string
+  role: string
+  yearsOfExperience: string
+  source: string
+  target: string
+  dueDate: string | undefined
+  skip: number
+  pageSize: number
+}
+
+export type FilterOmitType = Omit<FilterType, 'skip' | 'pageSize'>
+
+export const initialFilter: FilterOmitType = {
+  jobType: '',
+  role: '',
+  yearsOfExperience: '',
+  source: '',
+  target: '',
+  dueDate: '',
+}
 export default function jobPosting() {
-  return <div>여긴 잡포스팅~</div>
+  type FilterState = Array<{ value: string; label: string }>
+  const [filter, setFilter] = useState<FilterOmitType>({ ...initialFilter })
+  const [jobTypeOption, setJobTypeOption] = useState<FilterState>([])
+  const [roleOption, setRoleOption] = useState<FilterState>([])
+  const [skip, setSkip] = useState(1)
+  const [pageSize, setPageSize] = useState(10)
+  const [search, setSearch] = useState(true)
+
+  function findDynamicFilterOptions(
+    type: 'role' | 'jobType',
+  ): Array<{ value: string; label: string }> {
+    switch (type) {
+      case 'jobType':
+        const key = filter.jobType as keyof typeof ProRolePair
+        return ProRolePair[key]?.length ? [...ProRolePair[key]] : []
+      case 'role':
+        const roleKey = filter.role as keyof typeof ProJobPair
+        return ProJobPair[roleKey]?.length ? [...ProJobPair[roleKey]] : []
+      default:
+        return []
+    }
+  }
+
+  useEffect(() => {
+    const newFilter = findDynamicFilterOptions('jobType')
+    setRoleOption(newFilter)
+    if (newFilter.length && filter.role !== '') {
+      const reset = newFilter.filter(role => role.value === filter.role)
+      reset.length &&
+        setFilter({
+          ...filter,
+          role: reset[0].value,
+        })
+    }
+  }, [filter.jobType])
+
+  useEffect(() => {
+    const newFilter = findDynamicFilterOptions('role')
+    setJobTypeOption(newFilter)
+    if (newFilter.length && filter.jobType !== '') {
+      const reset = newFilter.filter(
+        jobType => jobType.value === filter.jobType,
+      )
+      reset.length &&
+        setFilter({
+          ...filter,
+          jobType: reset[0].value,
+        })
+    }
+  }, [filter.role])
+
+  function onReset() {
+    setFilter({ ...initialFilter })
+  }
+
+  useEffect(() => {
+    if (isEqual(initialFilter, filter)) {
+      // refetch()
+    }
+  }, [filter])
+
+  return (
+    <Grid container spacing={6} className='match-height'>
+      <PageHeader title={<Typography variant='h5'>Job posting</Typography>} />
+      <Filters
+        filter={filter}
+        setFilter={setFilter}
+        jobTypeOption={jobTypeOption}
+        onReset={onReset}
+        roleOption={roleOption}
+        search={() => setSearch(true)}
+      />
+    </Grid>
+  )
 }
 
 jobPosting.acl = {
