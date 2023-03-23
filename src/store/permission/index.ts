49d1d0a4e9373a54b02aaeb8d24234f9dbe2c6a7
gayeon <gayeon@gayeonui-MacBookPro.local>
2023-03-23 16:24:39 +0900

    add permission mock data

--- src/store/permission/index.ts
+++ src/store/permission/index.ts
@@ -11,58 +11,187 @@ export const getPermission = createAsyncThunk(
   'permissions/gerPermissions',
   async (): Promise<PermissionObjectType> => {
     try {
-      const { data } = await axios.get(`/api/enough/a/role/map`)
-      return data
-      // return [
-      //   {
-      //     subject: 'members',
-      //     can: ['read', 'create', 'update', 'delete'],
-      //   },
-      //   {
-      //     subject: 'permission_request',
-      //     can: ['read', 'create', 'update', 'delete'],
-      //   },
-      //   {
-      //     subject: 'personalInfo_pro',
-      //     can: ['read', 'create', 'update', 'delete'],
-      //   },
-      //   {
-      //     subject: 'personalInfo_manager',
-      //     can: ['read', 'create', 'update', 'delete'],
-      //   },
-      //   {
-      //     subject: 'dashboards',
-      //     can: ['read', 'create', 'update', 'delete'],
-      //   },
-      //   {
-      //     subject: 'onboarding',
-      //     can: ['read', 'create', 'update', 'delete'],
-      //   },
-      //   {
-      //     subject: 'client_guideline',
-      //     can: ['read', 'create', 'update', 'delete'],
-      //   },
-      //   {
-      //     subject: 'contract',
-      //     can: ['read', 'create', 'update', 'delete'],
-      //   },
-      //   {
-      //     subject: 'recruiting',
-      //     can: ['read', 'create', 'update', 'delete'],
-      //   },
-      //   {
-      //     subject: 'job_posting',
-      //     can: ['read', 'create', 'update', 'delete'],
-      //   },
-      //   {
-      //     subject: 'pro',
-      //     can: ['read', 'create', 'update', 'delete'],
-      //   },
-      //   {
-      //     subject: 'certification_test',
-      //     can: ['read', 'create', 'update', 'delete'],
-      //   },
-      // ]
+      // const { data } = await axios.get(`/api/enough/a/role/map`)
+      // return data
+      return [
+        {
+          subject: 'permission_request',
+          can: 'read',
+        },
+        {
+          subject: 'permission_request',
+          can: 'update',
+        },
+        {
+          subject: 'personalInfo_pro',
+          can: 'update',
+        },
+        {
+          subject: 'personalInfo_manager',
+          can: 'update',
+        },
+        {
+          subject: 'members',
+          can: 'read',
+        },
+        {
+          subject: 'onboarding',
+          can: 'read',
+        },
+        {
+          subject: 'certification_test',
+          can: 'create',
+        },
+        {
+          subject: 'certification_test',
+          can: 'read',
+        },
+        {
+          subject: 'certification_test',
+          can: 'update',
+          option: {
+            authorId: 5,
+          },
+        },
+        {
+          subject: 'certification_test',
+          can: 'delete',
+          option: {
+            authorId: 5,
+          },
+        },
+        {
+          subject: 'recruiting',
+          can: 'create',
+        },
+        {
+          subject: 'recruiting',
+          can: 'read',
+        },
+        {
+          subject: 'recruiting',
+          can: 'update',
+          option: {
+            authorId: 5,
+          },
+        },
+        {
+          subject: 'job_posting',
+          can: 'create',
+        },
+        {
+          subject: 'job_posting',
+          can: 'read',
+        },
+        {
+          subject: 'job_posting',
+          can: 'update',
+        },
+        {
+          subject: 'job_posting',
+          can: 'delete',
+        },
+        {
+          subject: 'client_guideline',
+          can: 'read',
+        },
+        {
+          subject: 'client_guideline',
+          can: 'create',
+        },
+        {
+          subject: 'client_guideline',
+          can: 'update',
+          option: {
+            authorId: 5,
+          },
+        },
+        {
+          subject: 'client_guideline',
+          can: 'delete',
+          option: {
+            authorId: 5,
+          },
+        },
+        {
+          subject: 'contract',
+          can: 'read',
+        },
+        {
+          subject: 'contract',
+          can: 'create',
+        },
+        {
+          subject: 'contract',
+          can: 'update',
+          option: {
+            authorId: 5,
+          },
+        },
+        {
+          subject: 'contract',
+          can: 'delete',
+          option: {
+            authorId: 5,
+          },
+        },
+        {
+          subject: 'personalInfo_pro',
+          can: 'read',
+        },
+        {
+          subject: 'onboarding_comment',
+          can: 'update',
+          option: {
+            authorId: 5,
+          },
+        },
+        {
+          subject: 'onboarding_comment',
+          can: 'delete',
+          option: {
+            authorId: 5,
+          },
+        },
+        {
+          subject: 'pro_comment',
+          can: 'create',
+        },
+        {
+          subject: 'pro_comment',
+          can: 'update',
+          option: {
+            authorId: 5,
+          },
+        },
+        {
+          subject: 'pro_comment',
+          can: 'read',
+        },
+        {
+          subject: 'pro_comment',
+          can: 'delete',
+          option: {
+            authorId: 5,
+          },
+        },
+        {
+          subject: 'pro',
+          can: 'read',
+        },
+        {
+          subject: 'pro',
+          can: 'create',
+        },
+        {
+          subject: 'pro',
+          can: 'update',
+        },
+        {
+          subject: 'pro',
+          can: 'delete',
+        },
+      ]
     } catch (e: any) {
       throw new Error('getPermission error : ', e)
     }
