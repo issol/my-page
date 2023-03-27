6e297d9fb9d1fee51898e1dd8badb656fd9b99d5
LerielKim <85856680+LerielKim@users.noreply.github.com>
2023-03-27 16:03:07 +0900

    [Mod] applied role list

--- src/shared/const/role/roles.ts
+++ src/shared/const/role/roles.ts
@@ -240,17 +240,45 @@ export const OnboardingListRolePair: Array<RoleSelectType> = [
 
 export const AssignJobType = {
   'Documents/Text': [
+    { label: Role.DTPer, value: Role.DTPer },
+    { label: Role['DTP QCer'], value: Role['DTP QCer'] },
+    { label: Role.QCer, value: Role.QCer },
+    { label: Role.Transcriber, value: Role.Transcriber },
+    { label: Role.Translator, value: Role.Translator },
     { label: 'Localization engineer', value: 'Localization engineer' },
     { label: 'Reviewer', value: 'Reviewer' },
   ],
-  Dubbing: [{ label: 'Reviewer', value: 'Reviewer' }],
-  Interpretation: [{ label: 'Reviewer', value: 'Reviewer' }],
+  Dubbing: [
+    { label: 'Reviewer', value: 'Reviewer' },
+    { label: Role['Audio describer'], value: Role['Audio describer'] },
+    {
+      label: Role['Audio description QCer'],
+      value: Role['Audio description QCer'],
+    },
+    { label: Role['Dubbing audio QCer'], value: Role['Dubbing audio QCer'] },
+    { label: Role['Dubbing script QCer'], value: Role['Dubbing script QCer'] },
+    {
+      label: Role['Dubbing script translator'],
+      value: Role['Dubbing script translator'],
+    },
+    {
+      label: Role['Dubbing voice artist'],
+      value: Role['Dubbing voice artist'],
+    },
+  ],
+  Interpretation: [
+    { label: 'Reviewer', value: 'Reviewer' },
+    { label: Role.Interpreter, value: Role.Interpreter },
+  ],
   'Misc.': [
     { label: 'Content LQA reviewer', value: 'Content LQA reviewer' },
     { label: 'DTT reviewer', value: 'DTT reviewer' },
     { label: 'Language manager', value: 'Language manager' },
     { label: 'Reviewer', value: 'Reviewer' },
     { label: 'Vendor LM', value: 'Vendor LM' },
+    { label: Role.Copywriter, value: Role.Copywriter },
+    { label: Role.Editor, value: Role.Editor },
+    { label: Role['Video editor'], value: Role['Video editor'] },
   ],
 
   'OTT/Subtitle': [
@@ -259,13 +287,39 @@ export const AssignJobType = {
     { label: 'Language manager', value: 'Language manager' },
     { label: 'Reviewer', value: 'Reviewer' },
     { label: 'Vendor LM', value: 'Vendor LM' },
+    { label: Role.QCer, value: Role.QCer },
+    { label: Role['SDH author'], value: Role['SDH author'] },
+    { label: Role['SDH QCer'], value: Role['SDH QCer'] },
+    { label: Role['Subtitle author'], value: Role['Subtitle author'] },
+    { label: Role['Subtitle QCer'], value: Role['Subtitle QCer'] },
+    { label: Role['Supp author'], value: Role['Supp author'] },
+    { label: Role['Supp QCer'], value: Role['Supp QCer'] },
+    { label: Role['Template author'], value: Role['Template author'] },
+    { label: Role['Template QCer'], value: Role['Template QCer'] },
+    { label: Role.Transcriber, value: Role.Transcriber },
+    { label: Role.Translator, value: Role.Translator },
   ],
   Webcomics: [
     { label: 'Localization engineer', value: 'Localization engineer' },
     { label: 'Reviewer', value: 'Reviewer' },
+    { label: Role.DTPer, value: Role.DTPer },
+    { label: Role['DTP QCer'], value: Role['DTP QCer'] },
+    { label: Role.Proofreader, value: Role.Proofreader },
+    { label: Role['Webcomics QCer'], value: Role['Webcomics QCer'] },
+    {
+      label: Role['Webcomics translator'],
+      value: Role['Webcomics translator'],
+    },
   ],
   Webnovel: [
     { label: 'Localization engineer', value: 'Localization engineer' },
     { label: 'Reviewer', value: 'Reviewer' },
+    { label: Role.DTPer, value: Role.DTPer },
+    { label: Role['DTP QCer'], value: Role['DTP QCer'] },
+    { label: Role['Webnovel QCer'], value: Role['Webnovel QCer'] },
+    {
+      label: Role['Webcomics translator'],
+      value: Role['Webcomics translator'],
+    },
   ],
 } as const
