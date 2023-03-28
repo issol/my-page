31dbfd686ee34159eb204814391ec1e526129aac
LerielKim <85856680+LerielKim@users.noreply.github.com>
2023-03-28 20:56:15 +0900

    feat mode

--- next.config.js
+++ next.config.js
@@ -16,6 +16,7 @@ const withTM = require('next-transpile-modules')([
 module.exports = withTM({
   trailingSlash: true,
   reactStrictMode: false,
+  mode: 'development',
   optimization: {
     minimize: false,
   },
