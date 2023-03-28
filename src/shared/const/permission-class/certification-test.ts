387370dbdc920ffc861191ead812d523705806f2
LerielKim <85856680+LerielKim@users.noreply.github.com>
2023-03-28 22:21:03 +0900

    define name

--- src/shared/const/permission-class/certification-test.ts
+++ src/shared/const/permission-class/certification-test.ts
@@ -2,6 +2,9 @@ class certification_test {
   authorId: number
   constructor(authorId: number) {
     this.authorId = authorId
+    Object.defineProperty(certification_test.prototype.constructor, 'name', {
+      value: 'certification_test',
+    })
   }
 }
 
