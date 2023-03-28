0b5ad8b879d66af9843540be52e85eaadd0403bd
seoulsaram <kasts91@naver.com>
2023-03-28 15:11:15 +0900

    add/fix timezone helper, date helper

--- src/shared/helpers/date.helper.ts
+++ src/shared/helpers/date.helper.ts
@@ -1,23 +1,46 @@
 import { CountryType } from '@src/types/sign/personalInfoTypes'
 import dayjs from 'dayjs'
-import { getTimezone } from './timezone.helper'
+import { convertCountryCodeToTimezone, getTimezone } from './timezone.helper'
 
-// output ex : 01/31/2023, 12:40 AM (KR)
-// export function FullDateTimezoneHelper(value: any): string | undefined {
-//   if (value === undefined || value === null) return '-'
-//   const rtn: any = dayjs(value).format('MM/DD/YYYY, hh:mm A')
+export function convertDateByTimezone(date: string, from: string, to: string) {
+  /**
+   * from이 US, to가 KR일 경우, date를 US시간(미국)에서 KR시간(한국)으로 convert한다.
+   * output ex : 03/28/2023, 02:04 AM
+   * @param date Date형태의 string
+   * @param from KR, US와 같은 country code
+   * @param to KR, US와 같은 country code
+   */
 
-//   return `${rtn} (${getTimezone(value)})`
-// }
+  const toTimezone = convertCountryCodeToTimezone(to)
+  const fromDate = FullDateTimezoneHelper(date, from)
+    ? new Date(FullDateTimezoneHelper(date, from))
+    : new Date(date)
 
+  const convertedDate = new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    timeZone: toTimezone,
+  }).format(fromDate)
+
+  return FullDateHelper(convertedDate)
+}
+
+// output ex : 03/28/2023, 12:00 AM (EST)
 export function FullDateTimezoneHelper(
   value: any,
-  timezone: CountryType,
-): string | undefined {
+  timezone: CountryType | string,
+): string {
   if (value === undefined || value === null) return '-'
   const rtn: any = dayjs(value).format('MM/DD/YYYY, hh:mm A')
-
-  return `${rtn} (${getTimezone(value, timezone.code)})`
+  if (typeof timezone !== 'string' && timezone?.code)
+    return `${rtn} (${getTimezone(value, timezone.code)})`
+  else if (typeof timezone === 'string')
+    return `${rtn} (${getTimezone(value, timezone)})`
+  return '-'
 }
 
 // output ex : 01/31/2023
