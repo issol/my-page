6a1904332c3c167c8e143b321f407899a385065c
seoulsaram <kasts91@naver.com>
2023-04-03 16:50:51 +0900

    add company/price page

---
+++ src/pages/company/price/index.tsx
@@ -0,0 +1,92 @@
+// ** React Imports
+import { MouseEvent, SyntheticEvent, useState } from 'react'
+
+// ** MUI Imports
+import Tab from '@mui/material/Tab'
+import TabPanel from '@mui/lab/TabPanel'
+import TabContext from '@mui/lab/TabContext'
+import { styled } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
+import MuiTabList, { TabListProps } from '@mui/lab/TabList'
+
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+
+export default function Price() {
+  // ** State
+  const [value, setValue] = useState<string>('1')
+
+  const TabList = styled(MuiTabList)<TabListProps>(({ theme }) => ({
+    marginBottom: '24px',
+    '& .MuiTabs-indicator': {
+      display: 'none',
+    },
+    '& .Mui-selected': {
+      backgroundColor: theme.palette.primary.main,
+      color: `${theme.palette.common.white} !important`,
+    },
+    '& .MuiTab-root': {
+      minHeight: 38,
+      minWidth: 110,
+      borderRadius: 8,
+      paddingTop: theme.spacing(2),
+      paddingBottom: theme.spacing(2),
+      textTransform: 'none',
+    },
+  }))
+
+  const handleChange = (event: SyntheticEvent, newValue: string) => {
+    setValue(newValue)
+  }
+  return (
+    <TabContext value={value}>
+      <TabList onChange={handleChange} aria-label='Company price menu'>
+        <Tab
+          value='1'
+          label='Standard client prices'
+          iconPosition='start'
+          icon={<Icon icon='material-symbols:person-outline' />}
+          sx={{ marginLeft: '24px' }}
+        />
+        <Tab
+          value='2'
+          label='Standard Pro prices'
+          iconPosition='start'
+          icon={<Icon icon='ic:baseline-people-outline' />}
+          sx={{ marginLeft: '24px' }}
+        />
+        <Tab
+          value='3'
+          label='Price units'
+          iconPosition='start'
+          icon={<Icon icon='mdi:dollar' />}
+          sx={{ marginLeft: '24px' }}
+        />
+      </TabList>
+      <TabPanel value='1'>
+        <Typography>
+          Cake apple pie chupa chups biscuit liquorice tootsie roll liquorice
+          sugar plum. Cotton candy wafer wafer jelly cake caramels brownie
+          gummies.
+        </Typography>
+      </TabPanel>
+      <TabPanel value='2'>
+        <Typography>
+          Chocolate bar carrot cake candy canes sesame snaps. Cupcake pie gummi
+          bears jujubes candy canes. Chupa chups sesame snaps halvah.
+        </Typography>
+      </TabPanel>
+      <TabPanel value='3'>
+        <Typography>
+          Danish tiramisu jujubes cupcake chocolate bar cake cheesecake chupa
+          chups. Macaroon ice cream tootsie roll carrot cake gummi bears.
+        </Typography>
+      </TabPanel>
+    </TabContext>
+  )
+}
+
+Price.acl = {
+  action: 'read',
+  subject: 'company_price',
+}
