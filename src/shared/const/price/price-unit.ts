857bae06906ed7a6857f353b77cafcb0a83b46a3
seoulsaram <kasts91@naver.com>
2023-04-05 17:54:22 +0900

    add price unit validation schema & price-unit enum

---
+++ src/shared/const/price/price-unit.ts
@@ -0,0 +1,24 @@
+import { PriceUnit } from './price-unit.enum'
+
+export const PriceUnits = [
+  { label: PriceUnit['CAT discount'], value: PriceUnit['CAT discount'] },
+  { label: PriceUnit['Characters'], value: PriceUnit['Characters'] },
+  { label: PriceUnit['Days'], value: PriceUnit['Days'] },
+  { label: PriceUnit['Document'], value: PriceUnit['Document'] },
+  { label: PriceUnit['Fixed rate'], value: PriceUnit['Fixed rate'] },
+  { label: PriceUnit['Graphics'], value: PriceUnit['Graphics'] },
+  { label: PriceUnit['Hours'], value: PriceUnit['Hours'] },
+  { label: PriceUnit['Info'], value: PriceUnit['Info'] },
+  { label: PriceUnit['Lines'], value: PriceUnit['Lines'] },
+  { label: PriceUnit['Minimum price'], value: PriceUnit['Minimum price'] },
+  { label: PriceUnit['Minutes'], value: PriceUnit['Minutes'] },
+  { label: PriceUnit['Months'], value: PriceUnit['Months'] },
+  { label: PriceUnit['Pages'], value: PriceUnit['Pages'] },
+  { label: PriceUnit['Paragraphs'], value: PriceUnit['Paragraphs'] },
+  { label: PriceUnit['Percent'], value: PriceUnit['Percent'] },
+  { label: PriceUnit['Segment'], value: PriceUnit['Segment'] },
+  { label: PriceUnit['Term'], value: PriceUnit['Term'] },
+  { label: PriceUnit['Unit'], value: PriceUnit['Unit'] },
+  { label: PriceUnit['Words'], value: PriceUnit['Words'] },
+  { label: PriceUnit['Years'], value: PriceUnit['Years'] },
+]
---
+++ src/types/schema/price-unit.schema.ts
@@ -0,0 +1,41 @@
+import * as yup from 'yup'
+import { FormErrors } from 'src/shared/const/formErrors'
+
+export type PriceFormType = {
+  isBasePrice: boolean
+  priceUnit: string
+  unit: string
+  weighting: number | null
+  isActive: boolean
+  subPrice?: Array<{
+    isSubPrice: boolean
+    priceUnit: string
+    unit: string
+    weighting: null | number
+  }>
+}
+export const priceUnitSchema = yup.object().shape({
+  isBasePrice: yup.boolean().required(),
+  priceUnit: yup.string().required(),
+  unit: yup.string().nullable(),
+  weighting: yup
+    .number()
+    .nullable()
+    .when('isBasePrice', (isBasePrice, schema) =>
+      !isBasePrice ? schema : yup.number().required(),
+    ),
+  subPrice: yup
+    .array()
+    .min(1, FormErrors.required)
+    .of(
+      yup.object().shape({
+        isSubPrice: yup.boolean(),
+        priceUnit: yup.string().required(),
+        unit: yup.string().nullable(),
+        weighting: yup.number().min(1).required(),
+      }),
+    )
+    .when('isBasePrice', (isBasePrice, schema) => {
+      return isBasePrice ? schema : yup.array().nullable()
+    }),
+})
