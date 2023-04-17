a4e8197dbaf48746b16a035d3b8e791f801d22fd
LerielKim <85856680+LerielKim@users.noreply.github.com>
2023-04-17 23:44:44 +0900

    [Mod] lang pair schema

--- src/types/schema/price-unit.schema.ts
+++ src/types/schema/price-unit.schema.ts
@@ -46,11 +46,40 @@ export const languagePairSchema = yup.object().shape({
       source: yup.string().required(FormErrors.required),
       target: yup.string().required(FormErrors.required),
       priceFactor: yup
-        .number()
-        .nullable()
-        .required(FormErrors.required)
-        .typeError('Invalid number'),
-      minimumPrice: yup.number().nullable().typeError('Invalid number'),
+        .mixed()
+        .test('is-valid-input', 'Invalid number', value => {
+          const regex = /^-?(?!.*--)[\d.-]+$/
+
+          return !value || regex.test(value.toString()) // 입력값이 falsy인 경우나 숫자형이면 통과
+        })
+        .test('is-integer-or-decimal', 'Invalid number', value => {
+          const regex = /^-?(?!.*--)[\d.-]+$/
+          return !value || regex.test(value.toString()) // 입력값이 falsy인 경우나 숫자형이면 통과
+        })
+        .test('valid-number', 'Invalid number', value => {
+          if (value === '-') {
+            return true
+          }
+          return !value || !isNaN(Number(value))
+        })
+        .required(FormErrors.required),
+      minimumPrice: yup
+        .mixed()
+        .test('is-valid-input', 'Invalid number', value => {
+          const regex = /^-?(?!.*--)[\d.-]+$/
+
+          return !value || regex.test(value.toString()) // 입력값이 falsy인 경우나 숫자형이면 통과
+        })
+        .test('is-integer-or-decimal', 'Invalid number', value => {
+          const regex = /^-?(?!.*--)[\d.-]+$/
+          return !value || regex.test(value.toString()) // 입력값이 falsy인 경우나 숫자형이면 통과
+        })
+        .test('valid-number', 'Invalid number', value => {
+          if (value === '-') {
+            return true
+          }
+          return !value || !isNaN(Number(value))
+        }),
     }),
   ),
 })
