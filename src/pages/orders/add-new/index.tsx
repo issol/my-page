44529a623c36b694f4466309b1ec266c2634bc00
gayeon <gayeon@gayeonui-MacBookPro.local>
2023-04-25 00:33:12 +0900

    add orders step 1,2 forms

---
+++ src/pages/orders/add-new/index.tsx
@@ -0,0 +1,183 @@
+import { useEffect, useRef, useState } from 'react'
+import { useRouter } from 'next/router'
+
+// ** hooks
+import useModal from '@src/hooks/useModal'
+
+// ** mui
+import { Box, Card, Grid, IconButton, Typography } from '@mui/material'
+import PageHeader from '@src/@core/components/page-header'
+
+import { useForm, Controller, useFieldArray } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+
+// ** third parties
+import { toast } from 'react-hot-toast'
+
+// ** validation values & types
+import {
+  ProjectTeamType,
+  projectTeamSchema,
+} from '@src/types/schema/project-team.schema'
+import { ClientFormType, clientSchema } from '@src/types/schema/client.schema'
+
+// ** components
+import PageLeaveModal from '@src/pages/client/components/modals/page-leave-modal'
+import Stepper from '@src/pages/components/stepper'
+import ProjectTeamFormContainer from '@src/pages/quotes/components/form-container/project-team-container'
+import ClientQuotesFormContainer from '@src/pages/quotes/components/form-container/client-container'
+
+export default function AddNewQuotes() {
+  const router = useRouter()
+
+  const { openModal, closeModal } = useModal()
+
+  // ** stepper
+  const [activeStep, setActiveStep] = useState<number>(0)
+
+  const handleBack = () => {
+    setActiveStep(prevActiveStep => prevActiveStep - 1)
+  }
+
+  const onNextStep = () => {
+    setActiveStep(activeStep + 1)
+  }
+
+  const steps = [
+    {
+      title: 'Project team',
+    },
+    {
+      title: 'Client',
+    },
+    {
+      title: 'Project info',
+    },
+    {
+      title: ' Languages & Items',
+    },
+  ]
+
+  // ** confirm page leaving
+  router.beforePopState(() => {
+    openModal({
+      type: 'alert-modal',
+      children: (
+        <PageLeaveModal
+          onClose={() => closeModal('alert-modal')}
+          onClick={() => router.push('/client')}
+        />
+      ),
+    })
+    return false
+  })
+
+  // ** step1
+  const {
+    control: teamControl,
+    getValues: getTeamValues,
+    setValue: setTeamValues,
+    handleSubmit: submitTeam,
+    watch: teamWatch,
+    formState: { errors: teamErrors, isValid: isTeamValid },
+  } = useForm<ProjectTeamType>({
+    mode: 'onChange',
+    defaultValues: {
+      teams: [
+        { type: 'supervisorId', id: null },
+        { type: 'projectManagerId', id: null },
+        { type: 'member', id: null },
+      ],
+    },
+    resolver: yupResolver(projectTeamSchema),
+  })
+
+  const {
+    fields: members,
+    append: appendMember,
+    remove: removeMember,
+    update: updateMember,
+  } = useFieldArray({
+    control: teamControl,
+    name: 'teams',
+  })
+
+  // ** step2
+  const {
+    control: clientControl,
+    getValues: getClientValue,
+    setValue: setClientValue,
+    watch: clientWatch,
+    formState: { errors: clientErrors, isValid: isClientValid },
+  } = useForm<ClientFormType>({
+    mode: 'onChange',
+    defaultValues: {
+      clientId: null,
+      contactPersonId: null,
+      addressType: 'shipping',
+    },
+    resolver: yupResolver(clientSchema),
+  })
+
+  return (
+    <Grid container spacing={6}>
+      <PageHeader
+        title={
+          <Box display='flex' alignItems='center' gap='8px'>
+            <IconButton onClick={() => router.back()}>
+              <Icon icon='material-symbols:arrow-back-ios-new-rounded' />
+            </IconButton>
+            <Typography variant='h5'>Create new order</Typography>
+          </Box>
+        }
+      />
+      <Grid item xs={12}>
+        <Stepper activeStep={activeStep} steps={steps} />
+      </Grid>
+      <Grid item xs={12}>
+        {activeStep === 0 ? (
+          <Card sx={{ padding: '24px' }}>
+            <ProjectTeamFormContainer
+              control={teamControl}
+              field={members}
+              append={appendMember}
+              remove={removeMember}
+              update={updateMember}
+              getValues={getTeamValues}
+              setValue={setTeamValues}
+              errors={teamErrors}
+              isValid={isTeamValid}
+              watch={teamWatch}
+              onNextStep={onNextStep}
+            />
+          </Card>
+        ) : activeStep === 1 ? (
+          <Card sx={{ padding: '24px' }}>
+            <ClientQuotesFormContainer
+              control={clientControl}
+              getValues={getClientValue}
+              setValue={setClientValue}
+              errors={clientErrors}
+              isValid={isClientValid}
+              watch={clientWatch}
+              handleBack={handleBack}
+              onNextStep={onNextStep}
+            />
+          </Card>
+        ) : activeStep === 2 ? (
+          <Card sx={{ padding: '24px' }}>Project Info</Card>
+        ) : (
+          <Card sx={{ padding: '24px' }}>Languages & Items</Card>
+        )}
+      </Grid>
+    </Grid>
+  )
+}
+
+AddNewQuotes.acl = {
+  subject: 'quotes',
+  action: 'create',
+}
