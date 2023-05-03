ec5e69be0e23c1ec141d22c62087afd396ce0ba6
LerielKim <leriel@glocalizeinc.com>
2023-05-03 14:11:19 +0900

    [Mod] memoForPrice type

--- src/types/common/standard-price.ts
+++ src/types/common/standard-price.ts
@@ -13,7 +13,7 @@ export type StandardPriceListType = {
 
   decimalPlace: number
   roundingProcedure: string
-  memoForPrice?: string
+  memoForPrice?: string | undefined
   languagePairs: Array<LanguagePairListType>
   priceUnit: Array<PriceUnitListType>
   catInterface: {
