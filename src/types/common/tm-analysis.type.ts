fbb7dacd9f75c9062391f719bdf6671271320373
gayeon <gayeon@gayeonui-MacBookPro.local>
2023-05-08 18:50:50 +0900

    fix CAT tool data type & mock data

---
+++ src/types/common/tm-analysis.type.ts
@@ -0,0 +1,56 @@
+export type MemSourceType = {
+  toolName: 'memesource' | 'memoq'
+  calculationBasis: Array<'Words' | 'Character'>
+  targetLanguage: string | null
+  data: Array<MemSourceData>
+}
+
+export type MemoQType = {
+  toolName: 'memesource' | 'memoq'
+  calculationBasis: Array<'Words' | 'Character'>
+  targetLanguage: string | null
+  data: Array<MemoQData>
+}
+
+export type MemSourceInterface =
+  | 'Context Match'
+  | 'Repetitions'
+  | '100%'
+  | '95% - 99%'
+  | '85% - 94%'
+  | '75% - 84%'
+  | '50% - 74%'
+  | 'No Match'
+  | 'Total'
+export type MemSourceData = {
+  File: string
+  'Chars/Word': string
+  'Tagging Errors': string
+} & {
+  [K in MemSourceInterface]: TMDataType
+}
+
+export type MemoQInterface =
+  | 'X-translated'
+  | '101%'
+  | 'Repetitions'
+  | '100%'
+  | '95% - 99%'
+  | '85% - 94%'
+  | '75% - 84%'
+  | '50% - 74%'
+  | 'No Match'
+  | 'Fragments'
+  | 'Total'
+export type MemoQData = {
+  File: string
+  'Chars/Word': string
+} & {
+  [K in MemoQInterface]: TMDataType
+}
+
+export type TMDataType = {
+  Words?: string
+  Characters?: string
+  Percent: string
+}
