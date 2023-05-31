86e46ad56252d49853bf168341d16c995267e734
Jay Lee <jay@glozinc.com>
2023-05-31 17:06:38 +0900

    [ticket] 알람 목데이터 제거

--- src/layouts/components/horizontal/AppBarContent.tsx
+++ src/layouts/components/horizontal/AppBarContent.tsx
@@ -24,48 +24,48 @@ interface Props {
 }
 
 const notifications: NotificationsType[] = [
-  {
-    meta: 'Today',
-    avatarAlt: 'Flora',
-    title: 'Congratulation Flora! 🎉',
-    avatarImg: '/images/avatars/4.png',
-    subtitle: 'Won the monthly best seller badge',
-  },
-  {
-    meta: 'Yesterday',
-    avatarColor: 'primary',
-    subtitle: '5 hours ago',
-    avatarText: 'Robert Austin',
-    title: 'New user registered.',
-  },
-  {
-    meta: '11 Aug',
-    avatarAlt: 'message',
-    title: 'New message received 👋🏻',
-    avatarImg: '/images/avatars/5.png',
-    subtitle: 'You have 10 unread messages',
-  },
-  {
-    meta: '25 May',
-    title: 'Paypal',
-    avatarAlt: 'paypal',
-    subtitle: 'Received Payment',
-    avatarImg: '/images/misc/paypal.png',
-  },
-  {
-    meta: '19 Mar',
-    avatarAlt: 'order',
-    title: 'Received Order 📦',
-    avatarImg: '/images/avatars/3.png',
-    subtitle: 'New order received from John',
-  },
-  {
-    meta: '27 Dec',
-    avatarAlt: 'chart',
-    subtitle: '25 hrs ago',
-    avatarImg: '/images/misc/chart.png',
-    title: 'Finance report has been generated',
-  },
+  // {
+  //   meta: 'Today',
+  //   avatarAlt: 'Flora',
+  //   title: 'Congratulation Flora! 🎉',
+  //   avatarImg: '/images/avatars/4.png',
+  //   subtitle: 'Won the monthly best seller badge',
+  // },
+  // {
+  //   meta: 'Yesterday',
+  //   avatarColor: 'primary',
+  //   subtitle: '5 hours ago',
+  //   avatarText: 'Robert Austin',
+  //   title: 'New user registered.',
+  // },
+  // {
+  //   meta: '11 Aug',
+  //   avatarAlt: 'message',
+  //   title: 'New message received 👋🏻',
+  //   avatarImg: '/images/avatars/5.png',
+  //   subtitle: 'You have 10 unread messages',
+  // },
+  // {
+  //   meta: '25 May',
+  //   title: 'Paypal',
+  //   avatarAlt: 'paypal',
+  //   subtitle: 'Received Payment',
+  //   avatarImg: '/images/misc/paypal.png',
+  // },
+  // {
+  //   meta: '19 Mar',
+  //   avatarAlt: 'order',
+  //   title: 'Received Order 📦',
+  //   avatarImg: '/images/avatars/3.png',
+  //   subtitle: 'New order received from John',
+  // },
+  // {
+  //   meta: '27 Dec',
+  //   avatarAlt: 'chart',
+  //   subtitle: '25 hrs ago',
+  //   avatarImg: '/images/misc/chart.png',
+  //   title: 'Finance report has been generated',
+  // },
 ]
 
 const shortcuts: ShortcutsType[] = [
