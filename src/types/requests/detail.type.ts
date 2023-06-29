b038560e589e11e92a54ea13bcc2697354a66026
gayeon <gayeon@gayeonui-MacBookPro.local>
2023-06-29 12:05:43 +0900

    [Mod] lpm용 request data type추가

--- src/types/requests/detail.type.ts
+++ src/types/requests/detail.type.ts
@@ -1,11 +1,16 @@
+import { ClientRowType } from '@src/apis/client.api'
 import { ContactPersonType } from '../schema/client-contact-person.schema'
 import { RequestItemFormType, RequestStatusType } from './common.type'
+import { ClientAddressType } from '../schema/client-address.schema'
 
+//TODO: client에 대한 정보가 추가로 필요함
+//company name, id, address정보 등
 export type RequestDetailType = {
   id: number
   corporationId: number
   lsp: { id: string; name: string; email: string }
   contactPerson: ContactPersonType
+  client: ClientRowType & { addresses?: ClientAddressType[] } //lpm에게만 오는 데이터
   status: RequestStatusType
   items: Array<RequestItemFormType & { id: number }>
   sampleFiles?: Array<{
