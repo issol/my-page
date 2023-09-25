9431e746732cc475816bb89db2c9b5b86c143f69
LerielKim <leriel@glozinc.com>
2023-09-25 09:52:48 +0900

    [Feat] init pro invoice

---
+++ src/pages/invoice/pro/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+
+const ProInvoice = () => {
+  return <Box></Box>
+}
+
+export default ProInvoice
+
+ProInvoice.acl = {
+  subject: 'invoice_payable',
+  action: 'read',
+}
