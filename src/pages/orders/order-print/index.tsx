dbad1bb604b8d2f6f415c4ee6a1e91a25a4e7873
LerielKim <leriel@glozinc.com>
2023-10-17 11:16:01 +0900

    [Fix] G-3200

--- src/pages/orders/order-print/index.tsx
+++ src/pages/orders/order-print/index.tsx
@@ -1,19 +1,11 @@
-import { useSelector } from 'react-redux'
-import { Row } from '../order-list/detail/[id]'
 import PrintOrderPage from './print-page'
 import { useAppSelector } from '@src/hooks/useRedux'
-import { useRouter } from 'next/router'
-import {
-  useGetClient,
-  useGetLangItem,
-  useGetProjectInfo,
-  useGetProjectTeam,
-} from '@src/queries/order/order.query'
-import { ReactNode, useContext, useEffect, useState } from 'react'
-import { OrderDownloadData } from '@src/types/orders/order-detail'
+
+import { ReactNode } from 'react'
+
 import BlankLayout from '@src/@core/layouts/BlankLayout'
 import Error404 from '@src/pages/404'
-import { Box } from '@mui/material'
+
 import { useRecoilValueLoadable } from 'recoil'
 import { authState } from '@src/states/auth'
 
