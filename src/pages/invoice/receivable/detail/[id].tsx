4ffb6f607f51bf93a901bf2e88cc052c935797eb
Jay Lee <jay@glozinc.com>
2023-10-20 12:25:52 +0900

    [HotFIx] Client 사이드에서 버튼 노출 제거

--- src/pages/invoice/receivable/detail/[id].tsx
+++ src/pages/invoice/receivable/detail/[id].tsx
@@ -919,7 +919,8 @@ const ReceivableInvoiceDetail = () => {
                     Download invoice
                   </Button>
                 )}
-                {isEditing ? null : (
+                {isEditing ||
+                (currentRole && currentRole.name === 'CLIENT')? null : (
                   <Button
                     variant='contained'
                     sx={{ display: 'flex', gap: '8px', alignItems: 'center' }}
