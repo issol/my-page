dce1e20e6b44e1bcb3b6e649df70d16b712dd2ef
LerielKim <leriel@glozinc.com>
2023-11-06 17:55:37 +0900

    [Fix] G-4074

--- src/pages/invoice/receivable/components/list/select-order.tsx
+++ src/pages/invoice/receivable/components/list/select-order.tsx
@@ -202,6 +202,8 @@ export default function SelectOrder({
               ),
             })
             setSelectionModel([])
+          } else {
+            setSelectionModel(selectionModel)
           }
         } else if (hasDifferentClient) {
           openModal({
