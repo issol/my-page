30560951cfcf6b4d2ae01d8dba08e0f657f9d165
LerielKim <leriel@glozinc.com>
2023-11-08 11:55:05 +0900

    [Fix] G-3578

--- src/pages/invoice/receivable/index.tsx
+++ src/pages/invoice/receivable/index.tsx
@@ -329,7 +329,7 @@ export default function Receivable() {
               <Typography>Hide paid invoices</Typography>
               <Switch
                 checked={hidePaidInvoices}
-                onChange={e => handleHidePaidInvoices}
+                onChange={handleHidePaidInvoices}
               />
             </Box>
           </Grid>
