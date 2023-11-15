e08d2bafd4277bde17243437d0adefd778f94d81
LerielKim <leriel@glozinc.com>
2023-11-15 09:51:19 +0900

    [Fix] G-4177

--- src/pages/orders/order-list/detail/components/client.tsx
+++ src/pages/orders/order-list/detail/components/client.tsx
@@ -94,7 +94,7 @@ const OrderDetailClient = ({
                 `${
                   client.contactPerson.jobTitle &&
                   client.contactPerson.jobTitle !== ''
-                    ? ' / '
+                    ? ` / ${client.contactPerson.jobTitle}`
                     : ''
                 }`}
             </Typography>
