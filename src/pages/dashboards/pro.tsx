b7feba17fbae2ff21ea78a5544ecab0d3d7d9b35
niki <niki@glozinc.com>
2023-12-20 15:50:14 +0900

    [Fix] 스티키헤더 적용

--- src/pages/dashboards/pro.tsx
+++ src/pages/dashboards/pro.tsx
@@ -62,169 +62,171 @@ const ProDashboards = () => {
   return (
     <FormProvider {...props} setValue={setValue} control={control}>
       <ApexChartWrapper>
-        <Grid
-          container
-          gap='24px'
-          sx={{ minWidth: '1320px', padding: '10px', overflow: 'auto' }}
-        >
+        <Grid container gap='24px' sx={{ padding: '10px' }}>
           <Notice />
-          <ChartDateHeader />
-        </Grid>
-        <Grid
-          container
-          gap='24px'
-          sx={{
-            minWidth: '1320px',
-            padding: '10px',
-          }}
-        >
-          <Grid container gap='24px'>
-            <GridItem width={265} height={387}>
-              <Box sx={{ width: '100%', height: '100%' }}>
-                <Title
-                  title='Job overview'
-                  openDialog={setOpenInfoDialog}
-                  handleClick={() => router.push('/jobs/')}
-                />
-                <JobList />
-              </Box>
-            </GridItem>
-            <GridItem sm height={387} padding='0px'>
-              <Box sx={{ width: '100%', height: '100%' }}>
-                <Title padding='20px' title='Upcoming deadlines' />
-                <DataGrid
-                  hideFooter
-                  components={{
-                    NoRowsOverlay: () => (
-                      <Stack
-                        height='50%'
-                        alignItems='center'
-                        justifyContent='center'
-                      >
-                        There are no deadlines
-                      </Stack>
-                    ),
-                  }}
-                  //upcomingData
-                  rows={upcomingData || []}
-                  columns={upcomingColumns}
-                  disableSelectionOnClick
-                  pagination={undefined}
-                />
-              </Box>
-            </GridItem>
+          <Grid
+            component='div'
+            item
+            xs={12}
+            sx={{
+              position: 'sticky',
+              left: 0,
+              top: '148px',
+              zIndex: 10,
+            }}
+          >
+            <ChartDateHeader />
           </Grid>
-          <Grid container gap='24px'>
-            <GridItem sm height={490} padding='0px'>
-              <Expectedincome
-                setOpenInfoDialog={setOpenInfoDialog}
-                dateRange={dateRange || [new Date(), new Date()]}
-              />
-            </GridItem>
-            <Doughnut<ServiceRatioItem>
-              title='Completed deliveries'
-              emptyTitle='delivery this month'
-              subTitle={`Based On ${getProDateFormat(
-                getDate('year'),
-                getDate('month'),
-              )}`}
-              height={490}
-              from={getDateFormat(
-                (Array.isArray(dateRange) && dateRange[0]) || null,
-              )}
-              to={getDateFormat(
-                (Array.isArray(dateRange) && dateRange[1]) || null,
-              )}
-              type='category'
-              colors={Colors}
-              getName={item => {
-                return `${item?.serviceType || '-'}`
-              }}
-              isHiddenValue={true}
-              path={`job/ratio/service-type?month=${getDate(
-                'month',
-              )}&year=${getDate('year')}`}
-              setOpenInfoDialog={setOpenInfoDialog}
-            />
-          </Grid>
-          <Grid display='flex' container>
-            <Grid
-              container
-              item
-              xs={6}
-              gap='24px'
-              sx={{ paddingRight: '24px' }}
-            >
-              <GridItem height={184}>
-                <CurrencyAmount
-                  title='Invoiced amount'
-                  amountType='invoiced'
-                  year={getDate('year')}
-                  month={getDate('month')}
-                  setOpenInfoDialog={setOpenInfoDialog}
-                />
-              </GridItem>
-              <GridItem height={184}>
-                <CurrencyAmount
-                  title='Payment amount'
-                  amountType='payment'
-                  year={getDate('year')}
-                  month={getDate('month')}
-                  setOpenInfoDialog={setOpenInfoDialog}
-                />
-              </GridItem>
-            </Grid>
 
-            <Grid container item xs={6}>
-              <GridItem height={392}>
+          <Grid container gap='24px'>
+            <Grid container gap='24px'>
+              <GridItem width={265} height={387}>
                 <Box sx={{ width: '100%', height: '100%' }}>
                   <Title
-                    title='Invoice overview'
-                    marginBottom='20px'
-                    subTitle={getProDateFormat(
-                      getDate('year'),
-                      getDate('month'),
-                    )}
+                    title='Job overview'
                     openDialog={setOpenInfoDialog}
+                    handleClick={() => router.push('/jobs/')}
                   />
-                  <InvoiceTab
-                    year={getDate('year')}
-                    month={getDate('month') + 1}
+                  <JobList />
+                </Box>
+              </GridItem>
+              <GridItem sm height={387} padding='0px'>
+                <Box sx={{ width: '100%', height: '100%' }}>
+                  <Title padding='20px' title='Upcoming deadlines' />
+                  <DataGrid
+                    hideFooter
+                    components={{
+                      NoRowsOverlay: () => (
+                        <Stack
+                          height='50%'
+                          alignItems='center'
+                          justifyContent='center'
+                        >
+                          There are no deadlines
+                        </Stack>
+                      ),
+                    }}
+                    //upcomingData
+                    rows={upcomingData || []}
+                    columns={upcomingColumns}
+                    disableSelectionOnClick
+                    pagination={undefined}
                   />
                 </Box>
               </GridItem>
             </Grid>
-          </Grid>
-
-          <Grid container gap='24px'>
-            <GridItem sm height={223} padding='0px'>
-              <Box
-                display='flex'
-                alignItems='center'
-                justifyContent='center'
-                sx={{ width: '100%', height: '100%', overflow: 'hidden' }}
-              >
-                <img
-                  style={{ width: '110%' }}
-                  alt='empty_imgage'
-                  src='/images/dashboard/img_montly.png'
+            <Grid container gap='24px'>
+              <GridItem sm height={490} padding='0px'>
+                <Expectedincome
+                  setOpenInfoDialog={setOpenInfoDialog}
+                  dateRange={dateRange || [new Date(), new Date()]}
                 />
-              </Box>
-            </GridItem>
-            <GridItem xs={6} height={223}>
-              <Deadline year={getDate('year')} month={getDate('month')} />
-            </GridItem>
+              </GridItem>
+              <Doughnut<ServiceRatioItem>
+                title='Completed deliveries'
+                emptyTitle='delivery this month'
+                subTitle={`Based On ${getProDateFormat(
+                  getDate('year'),
+                  getDate('month'),
+                )}`}
+                height={490}
+                from={getDateFormat(
+                  (Array.isArray(dateRange) && dateRange[0]) || null,
+                )}
+                to={getDateFormat(
+                  (Array.isArray(dateRange) && dateRange[1]) || null,
+                )}
+                type='category'
+                colors={Colors}
+                getName={item => {
+                  return `${item?.serviceType || '-'}`
+                }}
+                isHiddenValue={true}
+                path={`job/ratio/service-type?month=${getDate(
+                  'month',
+                )}&year=${getDate('year')}`}
+                setOpenInfoDialog={setOpenInfoDialog}
+              />
+            </Grid>
+            <Grid display='flex' container>
+              <Grid
+                container
+                item
+                xs={6}
+                gap='24px'
+                sx={{ paddingRight: '24px' }}
+              >
+                <GridItem height={184}>
+                  <CurrencyAmount
+                    title='Invoiced amount'
+                    amountType='invoiced'
+                    year={getDate('year')}
+                    month={getDate('month')}
+                    setOpenInfoDialog={setOpenInfoDialog}
+                  />
+                </GridItem>
+                <GridItem height={184}>
+                  <CurrencyAmount
+                    title='Payment amount'
+                    amountType='payment'
+                    year={getDate('year')}
+                    month={getDate('month')}
+                    setOpenInfoDialog={setOpenInfoDialog}
+                  />
+                </GridItem>
+              </Grid>
+
+              <Grid container item xs={6}>
+                <GridItem height={392}>
+                  <Box sx={{ width: '100%', height: '100%' }}>
+                    <Title
+                      title='Invoice overview'
+                      marginBottom='20px'
+                      subTitle={getProDateFormat(
+                        getDate('year'),
+                        getDate('month'),
+                      )}
+                      openDialog={setOpenInfoDialog}
+                    />
+                    <InvoiceTab
+                      year={getDate('year')}
+                      month={getDate('month') + 1}
+                    />
+                  </Box>
+                </GridItem>
+              </Grid>
+            </Grid>
+
+            <Grid container gap='24px'>
+              <GridItem sm height={223} padding='0px'>
+                <Box
+                  display='flex'
+                  alignItems='center'
+                  justifyContent='center'
+                  sx={{ width: '100%', height: '100%', overflow: 'hidden' }}
+                >
+                  <img
+                    style={{ width: '110%' }}
+                    alt='empty_imgage'
+                    src='/images/dashboard/img_montly.png'
+                  />
+                </Box>
+              </GridItem>
+              <GridItem xs={6} height={223}>
+                <Deadline year={getDate('year')} month={getDate('month')} />
+              </GridItem>
+            </Grid>
+            <Grid container>
+              <ProCalendar year={getDate('year')} month={getDate('month')} />
+            </Grid>
           </Grid>
-          <Grid container>
-            <ProCalendar year={getDate('year')} month={getDate('month')} />
-          </Grid>
+          <Information
+            open={isShowInfoDialog}
+            keyName={infoDialogKey}
+            infoType='PRO'
+            close={close}
+          />
         </Grid>
-        <Information
-          open={isShowInfoDialog}
-          keyName={infoDialogKey}
-          infoType='PRO'
-          close={close}
-        />
       </ApexChartWrapper>
     </FormProvider>
   )
