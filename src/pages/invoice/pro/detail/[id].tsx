d3f1fb52f74df08e06d8e3a36d59003ffbf06c09
LerielKim <leriel@glozinc.com>
2023-12-22 11:49:18 +0900

    [HotFix] invoice billing address

--- src/pages/invoice/pro/detail/[id].tsx
+++ src/pages/invoice/pro/detail/[id].tsx
@@ -129,13 +129,9 @@ const ProInvoiceDetail = () => {
           }),
           email: user?.email ?? '-',
           mobile: user?.mobilePhone,
-          address: {
-            detailAddress: '31-1',
-            baseAddress: 'Hangang-daero',
-            city: 'Yongsan-gu',
-            state: 'Seoul',
-            zipCode: '02148',
-          },
+          address:
+            user?.addresses.find(value => value.addressType === 'billing') ??
+            data.pro?.address,
         },
         jobList: jobList?.data || [],
         subtotal: data.subtotal,
