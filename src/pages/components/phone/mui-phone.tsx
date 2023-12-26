de03d410f80475320133e7d6b6f6db70ecc723e3
niki <niki@glozinc.com>
2023-12-26 14:35:10 +0900

    [Fix] 핸드폰 ui 레이아웃 수정 반영

--- src/pages/components/phone/mui-phone.tsx
+++ src/pages/components/phone/mui-phone.tsx
@@ -1,123 +1,117 @@
-import 'react-international-phone/style.css';
+import 'react-international-phone/style.css'
 import {
-    BaseTextFieldProps,
-    InputAdornment,
-    MenuItem,
-    Select,
-    TextField,
-    Typography,
-    Box,
-  } from '@mui/material';
-  import Divider from '@mui/material/Divider';
-  import React from 'react';
-  
-  import {
-    CountryIso2,
-    defaultCountries,
-    FlagImage,
-    parseCountry,
-    usePhoneInput,
-  } from 'react-international-phone';
+  BaseTextFieldProps,
+  InputAdornment,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+  Box,
+} from '@mui/material'
+import Divider from '@mui/material/Divider'
+import React from 'react'
 
-  export interface MUIPhoneProps extends BaseTextFieldProps {
-    value: string;
-    onChange: (phone: string) => void;
-  }
+import {
+  CountryIso2,
+  defaultCountries,
+  FlagImage,
+  parseCountry,
+  usePhoneInput,
+} from 'react-international-phone'
 
-  export default function MuiPhone({
-    value,
-    onChange,
-    ...restProps
-  }: MUIPhoneProps) {
-    const { inputValue, handlePhoneValueChange, inputRef, country, setCountry } =
-      usePhoneInput({
-        defaultCountry: 'kr',
-        disableFormatting: true,
-        forceDialCode: true,
-        value,
-        countries: defaultCountries,
-        onChange: (data) => {
-          onChange(data.inputValue.replace(' ','|').replace('+',''));
-        },
-      });
-  
-    return (
-      <TextField
-        sx={{
-          width: '100%'
-        }}
-        variant="outlined"
-        label="Phone number"
-        color="primary"
-        placeholder="Phone number"
-        value={inputValue}
-        onChange={handlePhoneValueChange}
-        type="tel"
-        inputRef={inputRef}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment
-              position="start"
-              style={{ marginRight: '2px', marginLeft: '-8px' }}
-            >
-              <Select
-                MenuProps={{
-                  style: {
-                    height: '300px',
-                    width: '360px',
-                    top: '10px',
-                    left: '-34px',
-                  },
-                  transformOrigin: {
-                    vertical: 'top',
-                    horizontal: 'left',
-                  },
-                }}
-                sx={{
-                  width: 'max-content',
-                  // Remove default outline (display only on focus)
+export interface MUIPhoneProps extends BaseTextFieldProps {
+  value: string
+  onChange: (phone: string) => void
+}
+
+export default function MuiPhone({
+  value,
+  onChange,
+  ...restProps
+}: MUIPhoneProps) {
+  const { inputValue, handlePhoneValueChange, inputRef, country, setCountry } =
+    usePhoneInput({
+      defaultCountry: 'kr',
+      disableFormatting: true,
+      forceDialCode: true,
+      value,
+      countries: defaultCountries,
+      onChange: data => {
+        onChange(data.inputValue.replace(' ', '|').replace('+', ''))
+      },
+    })
+
+  return (
+    <TextField
+      sx={{
+        width: '100%',
+      }}
+      variant='outlined'
+      label='Phone number'
+      color='primary'
+      placeholder='Phone number'
+      value={inputValue}
+      onChange={handlePhoneValueChange}
+      type='tel'
+      inputRef={inputRef}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position='start'>
+            <Select
+              MenuProps={{
+                style: {
+                  height: '300px',
+                  width: '360px',
+                  top: '6px',
+                  left: '-92px',
+                },
+                transformOrigin: {
+                  vertical: 'top',
+                  horizontal: 'left',
+                },
+              }}
+              sx={{
+                width: '80px',
+                fieldset: {
+                  display: 'none',
+                },
+                '&.Mui-focused:has(div[aria-expanded="false"])': {
                   fieldset: {
-                    display: 'none',
+                    display: 'block',
                   },
-                  '&.Mui-focused:has(div[aria-expanded="false"])': {
-                    fieldset: {
-                      display: 'block',
-                    },
-                  },
-                  // Update default spacing
-                  '.MuiSelect-select': {
-                    padding: '8px',
-                    paddingRight: '0px !important',
-                  },
-                  svg: {
-                    right: 0,
-                  },
-                }}
-                value={country.iso2}
-                onChange={(e) => setCountry(e.target.value as CountryIso2)}
-                renderValue={(value) => (
-                  <FlagImage iso2={value} style={{ display: 'flex' }} />
-                  
-                )}
-              >
-                {defaultCountries.map((c) => {
-                  const country = parseCountry(c);
-                  return (
-                    <MenuItem key={country.iso2} value={country.iso2}>
-                      <FlagImage
-                        iso2={country.iso2}
-                        style={{ marginRight: '8px' }}
-                      />
-                      <Typography marginRight="8px">{country.name}</Typography>
-                      <Typography color="gray">+{country.dialCode}</Typography>
-                    </MenuItem>
-                  );
-                })}
-              </Select>
-            </InputAdornment>
-          ),
-        }}
-        {...restProps}
-      />
-    );
-  };
+                },
+                // Update default spacing
+                '.MuiSelect-select': {
+                  left: '0 !important',
+                },
+                svg: {
+                  right: 0,
+                },
+              }}
+              value={country.iso2}
+              onChange={e => setCountry(e.target.value as CountryIso2)}
+              renderValue={value => (
+                <FlagImage iso2={value} style={{ display: 'flex' }} />
+              )}
+            >
+              {defaultCountries.map(c => {
+                const country = parseCountry(c)
+                return (
+                  <MenuItem key={country.iso2} value={country.iso2}>
+                    <FlagImage
+                      iso2={country.iso2}
+                      style={{ marginRight: '8px' }}
+                    />
+                    <Typography marginRight='8px'>{country.name}</Typography>
+                    <Typography color='gray'>+{country.dialCode}</Typography>
+                  </MenuItem>
+                )
+              })}
+            </Select>
+          </InputAdornment>
+        ),
+      }}
+      {...restProps}
+    />
+  )
+}
