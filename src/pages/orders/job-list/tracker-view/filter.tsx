893a176c5863f95e907198d5be3988deb71e8fad
LerielKim <leriel@glozinc.com>
2024-03-04 10:19:24 +0900

    [Mod] tracker view ui

--- src/pages/orders/job-list/tracker-view/filter.tsx
+++ src/pages/orders/job-list/tracker-view/filter.tsx
@@ -63,166 +63,169 @@ export default function Filters({
         )
   }
   return (
-    <Grid item xs={12}>
+    <Box sx={{ width: '100%' }}>
       <Card style={{ overflow: 'visible' }}>
-        <CardHeader
-          title='Search Filters'
-          action={
-            <IconButton
-              size='small'
-              aria-label='collapse'
-              sx={{ color: 'text.secondary' }}
-              onClick={() => setCollapsed(!collapsed)}
-            >
-              <Icon
-                fontSize={20}
-                icon={!collapsed ? 'mdi:chevron-down' : 'mdi:chevron-up'}
-              />
-            </IconButton>
-          }
-        />
-        <Collapse in={collapsed}>
-          <CardContent>
-            <Grid container spacing={6} rowSpacing={4}>
-              <Grid item xs={12} sm={6} md={6}>
-                <FormControl fullWidth>
-                  <FormControl fullWidth>
-                    <Autocomplete
-                      autoHighlight
-                      fullWidth
-                      multiple
-                      options={clients}
-                      value={clients.filter(client =>
-                        filter?.client?.includes(String(client.clientId)),
-                      )}
-                      onChange={(e, v) =>
-                        setFilter({
-                          ...filter,
-                          client: v.map(i => String(i.clientId)),
-                        })
-                      }
-                      // filterSelectedOptions
-                      getOptionLabel={option => option?.name}
-                      renderInput={params => (
-                        <TextField
-                          {...params}
-                          autoComplete='off'
-                          label='Client'
-                          placeholder='Client'
-                        />
-                      )}
-                      renderOption={(props, option, { selected }) => (
-                        <li {...props}>
-                          <Checkbox checked={selected} sx={{ mr: 2 }} />
-                          {option.name}
-                        </li>
-                      )}
+        <CardContent>
+          <Grid container spacing={6} rowSpacing={4}>
+            <Grid item xs={12} sm={6} md={6}>
+              <Box className='filterFormAutoComplete'>
+                <Autocomplete
+                  autoHighlight
+                  fullWidth
+                  multiple
+                  options={clients}
+                  value={clients.filter(client =>
+                    filter?.client?.includes(String(client.clientId)),
+                  )}
+                  onChange={(e, v) =>
+                    setFilter({
+                      ...filter,
+                      client: v.map(i => String(i.clientId)),
+                    })
+                  }
+                  // filterSelectedOptions
+                  getOptionLabel={option => option?.name}
+                  renderInput={params => (
+                    <TextField
+                      {...params}
+                      autoComplete='off'
+                      label='Client'
+                      placeholder='Client'
                     />
-                  </FormControl>
-                </FormControl>
-              </Grid>
-              <Grid item xs={12} sm={6} md={6}>
-                <FormControl fullWidth>
-                  <FormControl fullWidth>
-                    <Autocomplete
-                      fullWidth
-                      multiple
-                      options={CategoryList}
-                      value={filterValue(CategoryList, 'category')}
-                      onChange={(e, v) =>
-                        setFilter({
-                          ...filter,
-                          category: v.map(item => item.value),
-                        })
-                      }
-                      id='category'
-                      getOptionLabel={option => option.label}
-                      renderInput={params => (
-                        <TextField {...params} autoComplete='off' label='Category' />
-                      )}
-                      renderOption={(props, option, { selected }) => (
-                        <li {...props}>
-                          <Checkbox checked={selected} sx={{ mr: 2 }} />
-                          {option.label}
-                        </li>
-                      )}
-                    />
-                  </FormControl>
-                </FormControl>
-              </Grid>
-              <Grid item xs={12} sm={6} md={6}>
-                <FormControl fullWidth>
-                  <Autocomplete
-                    autoHighlight
-                    fullWidth
-                    multiple
-                    options={serviceTypeOptions || []}
-                    value={filterValue(ServiceTypeList, 'serviceType')}
-                    onChange={(e, v) =>
-                      setFilter({
-                        ...filter,
-                        serviceType: v.map(item => item.value),
-                      })
-                    }
-                    // filterSelectedOptions
-                    id='serviceType'
-                    getOptionLabel={option => option.label}
-                    renderInput={params => (
-                      <TextField
-                        {...params}
-                        autoComplete='off'
-                        label='Service type'
-                        placeholder='Service type'
-                      />
-                    )}
-                    renderOption={(props, option, { selected }) => (
-                      <li {...props}>
-                        <Checkbox checked={selected} sx={{ mr: 2 }} />
-                        {option.label}
-                      </li>
-                    )}
-                  />
-                </FormControl>
-              </Grid>
-              <Grid item xs={12} sm={6} md={6}>
-                <FormControl fullWidth>
-                  <InputLabel>Search projects</InputLabel>
-                  <OutlinedInput
-                    label='Search projects'
-                    value={filter.search}
-                    onChange={e =>
-                      setFilter({ ...filter, search: e.target.value })
-                    }
-                    endAdornment={
-                      <InputAdornment position='end'>
-                        <IconButton edge='end'>
-                          <Icon icon='mdi:magnify' />
-                        </IconButton>
-                      </InputAdornment>
-                    }
-                  />
-                </FormControl>
-              </Grid>
-              <Grid item xs={12}>
-                <Box display='flex' justifyContent='flex-end' gap='15px'>
-                  <Button
-                    variant='outlined'
-                    size='medium'
-                    color='secondary'
-                    type='button'
-                    onClick={onReset}
-                  >
-                    Reset
-                  </Button>
-                  <Button variant='contained' size='medium' onClick={onSearch}>
-                    Search
-                  </Button>
-                </Box>
-              </Grid>
+                  )}
+                  renderOption={(props, option, { selected }) => (
+                    <li {...props}>
+                      <Checkbox checked={selected} sx={{ mr: 2 }} />
+                      {option.name}
+                    </li>
+                  )}
+                />
+              </Box>
             </Grid>
-          </CardContent>
-        </Collapse>
+            <Grid item xs={12} sm={6} md={6}>
+              <Box className='filterFormAutoComplete'>
+                <Autocomplete
+                  fullWidth
+                  multiple
+                  options={CategoryList}
+                  value={filterValue(CategoryList, 'category')}
+                  onChange={(e, v) =>
+                    setFilter({
+                      ...filter,
+                      category: v.map(item => item.value),
+                    })
+                  }
+                  id='category'
+                  getOptionLabel={option => option.label}
+                  renderInput={params => (
+                    <TextField
+                      {...params}
+                      autoComplete='off'
+                      label='Category'
+                    />
+                  )}
+                  renderOption={(props, option, { selected }) => (
+                    <li {...props}>
+                      <Checkbox checked={selected} sx={{ mr: 2 }} />
+                      {option.label}
+                    </li>
+                  )}
+                />
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={6} md={6}>
+              <Box className='filterFormAutoComplete'>
+                <Autocomplete
+                  autoHighlight
+                  fullWidth
+                  multiple
+                  options={serviceTypeOptions || []}
+                  value={filterValue(ServiceTypeList, 'serviceType')}
+                  onChange={(e, v) =>
+                    setFilter({
+                      ...filter,
+                      serviceType: v.map(item => item.value),
+                    })
+                  }
+                  // filterSelectedOptions
+                  id='serviceType'
+                  getOptionLabel={option => option.label}
+                  renderInput={params => (
+                    <TextField
+                      {...params}
+                      autoComplete='off'
+                      label='Service type'
+                      placeholder='Service type'
+                    />
+                  )}
+                  renderOption={(props, option, { selected }) => (
+                    <li {...props}>
+                      <Checkbox checked={selected} sx={{ mr: 2 }} />
+                      {option.label}
+                    </li>
+                  )}
+                />
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <FormControl fullWidth className='filterFormControl'>
+                <InputLabel>Search projects</InputLabel>
+                <OutlinedInput
+                  label='Search projects'
+                  value={filter.search}
+                  onChange={e =>
+                    setFilter({ ...filter, search: e.target.value })
+                  }
+                  sx={{
+                    height: '46px',
+                  }}
+                  inputProps={{
+                    style: {
+                      height: '46px',
+                      padding: '0 14px',
+                    },
+                  }}
+                  endAdornment={
+                    <InputAdornment position='end'>
+                      <IconButton edge='end'>
+                        <Icon icon='mdi:magnify' />
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={12} md={2}>
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  height: '46px',
+                  gap: '16px',
+                }}
+              >
+                <Button
+                  variant='outlined'
+                  color='secondary'
+                  type='button'
+                  onClick={onReset}
+                  sx={{ flex: 1 }}
+                >
+                  Reset
+                </Button>
+                <Button
+                  variant='contained'
+                  color='secondary'
+                  sx={{ flex: 1 }}
+                  onClick={onSearch}
+                >
+                  Search
+                </Button>
+              </Box>
+            </Grid>
+          </Grid>
+        </CardContent>
       </Card>
-    </Grid>
+    </Box>
   )
 }
