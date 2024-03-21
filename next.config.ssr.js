cbe92489d56ac60c8b17349303aa2dbb8119c126
JayLee <jay@glozinc.com>
2024-03-21 10:01:38 +0900

    [Fix] SSR 테스트 설정 추가

--- next.config.ssr.js
+++ next.config.ssr.js
@@ -13,6 +13,7 @@ const nextConfig = {
     domains: ['profile-dev.gloground.com', 'localhost', '*', 'i.ytimg.com'],
     minimumCacheTTL: 3600,
   },
+  assetPrefix: `${process.env.NEXT_PUBLIC_HOST}/ssr/assets/`,
 }
 
 module.exports = nextConfig
