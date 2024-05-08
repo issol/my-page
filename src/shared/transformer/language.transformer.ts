ddd3cc0249b2465bbb42a428aa8d77a38aadac1d
LerielKim <leriel@glozinc.com>
2024-05-08 17:25:56 +0900

    [Mod] glo language version up

--- src/shared/transformer/language.transformer.ts
+++ src/shared/transformer/language.transformer.ts
@@ -2,10 +2,23 @@ import * as Type from '@glocalize-inc/glo-languages'
 
 //GloLanguageEnum
 export const getGloLanguage = () => {
-  return Object.entries(Type['GloLanguageEnum'])
-    .map(([value, label]) => ({
-      value,
-      label,
-    }))
-    .sort((a, b) => a.label.localeCompare(b.label))
+  return (
+    Object.entries(Type['GloLanguageEnum'])
+      .map(([value, label]) => {
+        if (label.isEnuffEnabled) {
+          return {
+            value,
+            label: label.name,
+          }
+        }
+      })
+      .filter(Boolean) as Array<{ value: string; label: string }>
+  ).sort((a, b) => a.label.localeCompare(b.label))
+
+  // return Object.entries(Type['GloLanguageEnum'])
+  //   .map(([value, label]) => ({
+  //     value,
+  //     label,
+  //   }))
+  //   .sort((a, b) => a.label.localeCompare(b.label))
 }
