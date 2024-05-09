75c1d5929bf711861e3723b54d110687717a58a5
JayLee <jay@glozinc.com>
2024-05-09 17:45:06 +0900

    [Test] allowed_parent_origin, state_cookie_domain 추가

--- src/pages/components/google-button/index.tsx
+++ src/pages/components/google-button/index.tsx
@@ -130,6 +130,8 @@ export default function GoogleButton({ type }: Props) {
       client_id:
         process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ||
         '644269375379-aidfbdlh5jip1oel3242h5al3o1qsr40.apps.googleusercontent.com',
+      allowed_parent_origin: ["https://*.gloground.com", "https://*.enuff.space"],
+      state_cookie_domain: 'gloground.com',
       callback: handleCredentialResponse,
     })
     //** 이거 활성화 하면 화면 오른쪽 상단에 구글 로그인이 보여짐 */
