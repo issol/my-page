779efe5f451981d002fbdaef339adb132b601461
LerielKim <leriel@glozinc.com>
2024-06-11 20:37:57 +0900

    [Fix] asset prefix

--- next.config.ssr.js
+++ next.config.ssr.js
@@ -13,7 +13,7 @@ const nextConfig = {
     domains: ['profile-dev.gloground.com', 'localhost', '*', 'i.ytimg.com'],
     minimumCacheTTL: 3600,
   },
-  assetPrefix: `${process.env.NEXT_PUBLIC_HOST}/ssr/assets/`,
+  assetPrefix: `${process.env.NEXT_PUBLIC_HOST}/`,
 }
 
 module.exports = nextConfig
